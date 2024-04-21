39518a669e86a88b0b44fd9d0e9b4eee429dc147
Joshua Loong <joshualoongwy@gmail.com>
2024-04-21 21:39:55 +1000

    feature/86cv5njry: created service details header component and request booking page

---
+++ behind-the-veil-siteroot/imports/ui/components/pages/RequestBooking.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Card from "../card/Card";
+import ServiceDetailsHeader from "../service-details-header/ServiceDetailsHeader";
+
+/**
+ * Page for user to request a booking
+ */
+const RequestBooking = () => {
+  const MOCK_SERVICE_DETAILS = {
+    service: "Bachelorette Glam Experience",
+    date: "Tuesday, 12 May, 2024",
+    artist: "Alice Tran",
+    price: "$120"
+  }
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="large-text">Request Booking</div>
+      <ServiceDetailsHeader details={MOCK_SERVICE_DETAILS}/>
+    </div>
+  );
+};
+
+export default RequestBooking;
