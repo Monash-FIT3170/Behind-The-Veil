b9c969b3cc56f6df34524fcfbe00ceaa3c968663
Nikki Li Shao <nsha0054@student.monash.edu>
2024-04-21 18:01:35 +1000

    feature/86cv4kz5a-navigation-bar: Added protected routes and messages page for routing

--- behind-the-veil-siteroot/imports/ui/App.jsx
+++ behind-the-veil-siteroot/imports/ui/App.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 import NavigationBar from './components/navigationBar/NavigationBar.jsx'
+import LoggedInOnlyRoute from "./components/protectedRoute/LoggedInOnlyRoute.jsx";
+import LoggedOutOnlyRoute from "./components/protectedRoute/LoggedOutOnlyRoute.jsx";
+
 import Examples from './components/pages/Examples.jsx'
 
-import {ArtistsPage} from './components/pages/ArtistsPage.jsx'
-import {HomePage} from './components/pages/HomePage.jsx'
-import {ServicesPage} from './components/pages/ServicesPage.jsx'
-import {LoginPage} from './components/pages/LoginPage.jsx'
-import {RegisterPage} from './components/pages/RegisterPage.jsx'
-
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import HomePage from './components/pages/HomePage.jsx'
+import ServicesPage from './components/pages/ServicesPage.jsx'
+import ArtistsPage from './components/pages/ArtistsPage.jsx'
+import MessagesPage from './components/pages/MessagesPage.jsx'
+import LoginPage from './components/pages/LoginPage.jsx'
+import RegisterPage from './components/pages/RegisterPage.jsx'
 
 export const App = () => (
 
@@ -19,16 +20,29 @@ export const App = () => (
 
         {/*This is the navigation bar on every Page*/}
         <Router>
-            <NavigationBar />
+            <NavigationBar/>
             <main className="main-content">
                 <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/services" element={<ServicesPage />} />
-                    <Route path="/artists" element={<ArtistsPage />} />
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/examples" element={<Examples/>}/> {/* removed once dev is finished*/}
 
-                    <Route path="/examples" element={<Examples />} />  {/* removed once dev is finished*/}
+                    {/*routes that any user can access*/}
+                    <Route path="/" element={<HomePage/>}/>
+                    <Route path="/services" element={<ServicesPage/>}/>
+                    <Route path="/artists" element={<ArtistsPage/>}/>
+
+                    {/*routes that NOT authenticated users can access*/}
+                    <Route path="/login"
+                           element={<LoggedOutOnlyRoute><LoginPage/></LoggedOutOnlyRoute>}/>
+
+                    <Route path="/register" element={<LoggedOutOnlyRoute><RegisterPage/></LoggedOutOnlyRoute>}/>
+
+                    {/*routes that ONLY authenticated users can access*/}
+                    <Route path="/messages"
+                           element={<LoggedInOnlyRoute> <MessagesPage/></LoggedInOnlyRoute>}/>
+
+                    {/* todo: not made account pages (bride and artist) thus, todo later*/}
+                    <Route path="/account"
+                           element={<LoggedInOnlyRoute> <MessagesPage/></LoggedInOnlyRoute>}/>
 
                     {/* Define other routes that you need*/}
                 </Routes>
@@ -39,7 +53,6 @@ export const App = () => (
         {/*<h1>Welcome to Meteor!</h1>*/}
         {/*<Hello/>*/}
         {/*<Info/>*/}
-
 
 
     </div>
---
+++ behind-the-veil-siteroot/imports/ui/components/pages/MessagesPage.jsx
@@ -0,0 +1,17 @@
+/**
+ * File Description: Messages page
+ * File version: 1.0
+ * Contributors:
+ */
+
+import React from 'react';
+
+export const MessagesPage = () => {
+    return (
+        <div className="flex flex-col gap-5 p-10">
+            <span>Messages Page to be done!!</span>
+        </div>
+    );
+};
+
+export default MessagesPage;
