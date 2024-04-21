dffca54c18a2344544e2f36adb5425ea237cc439
Joshua Loong <joshualoongwy@gmail.com>
2024-04-21 20:47:29 +1000

    feature/86cv5njry: button uplift, added forwardRef and type="button"

--- behind-the-veil-siteroot/imports/ui/components/button/Button.jsx
+++ behind-the-veil-siteroot/imports/ui/components/button/Button.jsx
@@ -1,19 +1,20 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import classNames from "classnames";
 
 /**
- * General button component
+ * General button component.
+ * forwardRef allows a component to accept a ref from a higher level component and assign it to a child component, see https://react.dev/reference/react/forwardRef
  * @param children children of the button component, e.g. text, icons, other components. See https://react.dev/learn/passing-props-to-a-component#passing-jsx-as-children
  * @param className custom classes that override the base button style, e.g. background color, font, etc
  * @param buttonProps encompasses all other props supplied and applies them to the button
  */
-const Button = ({ children, className, ...buttonProps }) => {
+const Button = forwardRef(({ children, className, ...buttonProps }, ref) => {
   const buttonClasses = classNames("btn-base", className);
   return (
-    <button {...buttonProps} className={buttonClasses}>
+    <button ref={ref} {...buttonProps} className={buttonClasses} type="button">
       {children}
     </button>
   );
-};
+});
 
 export default Button;
