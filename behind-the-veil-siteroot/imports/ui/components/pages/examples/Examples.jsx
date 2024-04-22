bf22aed986238ab22f174ff6a7edf28a9d3ff90d
Joshua Loong <joshualoongwy@gmail.com>
2024-04-22 20:45:22 +1000

    feature/86cv5njry: added Card examples

--- behind-the-veil-siteroot/imports/ui/components/pages/examples/Examples.jsx
+++ behind-the-veil-siteroot/imports/ui/components/pages/examples/Examples.jsx
@@ -6,42 +6,63 @@
 
 import React from "react";
 import Button from "../../button/Button";
+import Card from "../../card/Card";
 
 /**
  * Page to showcase examples
  */
 const Examples = () => {
-    // Example buttons
     return (
         <div className="flex flex-col gap-5 p-10">
             <div className="title-text">Examples</div>
-            <div className="large-text">Example buttons:</div>
-            <Button>Unstyled button</Button>
-            <Button className="bg-secondary-purple hover:bg-secondary-purple-hover">
-                Main purple with hover
-            </Button>
-            <Button className="bg-gradient-to-r from-bg-gradient-start to-bg-gradient-end">
-                Background gradient
-            </Button>
-            <Button onClick={() => console.log("Hello")}>
-                Click me and look at your console
-            </Button>
-            <div className="large-text">Font styles:</div>
-            <div className="title-text">title-text</div>
-            <div className="main-text">main-text</div>
-            <div className="large-text">large-text</div>
-            <div className="medium-text">medium-text</div>
-            <div className="small-text">small-text</div>
-            <div className="large-number-text">large-number-text</div>
-            <div className="logo-text">logo-text</div>
-            <div className="message-tag-text">message-tag-text</div>
-            <div className="message-receiver-name-text">
-                message-receiver-name-text
+            <div className="flex flex-col gap-3">
+                <div className="large-text underline">Buttons:</div>
+                <Button>Unstyled button</Button>
+                <Button className="bg-secondary-purple hover:bg-secondary-purple-hover">
+                    Main purple with hover
+                </Button>
+                <Button className="bg-gradient-to-r from-bg-gradient-start to-bg-gradient-end">
+                    Background gradient
+                </Button>
+                <Button onClick={() => console.log("Hello")}>
+                    Click me and look at your console
+                </Button>
             </div>
-            <div className="message-read-text">message-read-text</div>
-            <div className="message-unread-text">message-unread-text</div>
-            <div className="message-name-read-text">message-name-read-text</div>
-            <div className="message-name-unread-text">message-name-unread-text</div>
+            <div className="flex flex-col gap-3">
+                <div className="large-text underline">Font styles:</div>
+                <div className="title-text">title-text</div>
+                <div className="main-text">main-text</div>
+                <div className="large-text">large-text</div>
+                <div className="medium-text">medium-text</div>
+                <div className="small-text">small-text</div>
+                <div className="large-number-text">large-number-text</div>
+                <div className="logo-text">logo-text</div>
+                <div className="message-tag-text">message-tag-text</div>
+                <div className="message-receiver-name-text">
+                    message-receiver-name-text
+                </div>
+                <div className="message-read-text">message-read-text</div>
+                <div className="message-unread-text">message-unread-text</div>
+                <div className="message-name-read-text">message-name-read-text</div>
+                <div className="message-name-unread-text">message-name-unread-text</div>
+            </div>
+            <div className="flex flex-col gap-3">
+                <div className="large-text underline">Cards:</div>
+                <Card>
+                    This text is written on a card component.
+                </Card>
+                <Card className="bg-dark-grey text-white">
+                    You can also supply custom classes to this component to override the base styling if you really need to.
+                    To demonstrate, I've styled this card with a dark grey background and white text.
+                </Card>
+                <Card className="flex flex-col gap-2">
+                    Any component can be placed inside a card.
+                    For example, other divs:
+                    <div className="bg-light-grey w-fit p-2 rounded">Div with light grey background</div>
+                    Or buttons:
+                    <Button>Hello</Button>
+                </Card>
+            </div>
         </div>
     );
 };
