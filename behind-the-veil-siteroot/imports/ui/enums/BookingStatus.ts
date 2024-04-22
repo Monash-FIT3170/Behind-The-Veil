136b9fbaf58ab347e29b6f8e910c8e6cbad535fb
lzha0181 <lzha0181@student.monash.edu>
2024-04-22 16:57:04 +1000

    feature/86cuw4j8h Created BookingStatus enum

---
+++ behind-the-veil-siteroot/imports/ui/enums/BookingStatus.ts
@@ -0,0 +1,9 @@
+export enum BookingStatus {
+  PENDING = 'Pending',
+  CONFIRMED = 'Confirmed',
+  REJECTED = 'Rejected',
+  CANCELLED = 'Cancelled',
+  PENDING_CANCELLATION = 'Pending cancellation',
+  COMPLETED = 'completed',
+  OVERDUE = 'Overdue'
+}
