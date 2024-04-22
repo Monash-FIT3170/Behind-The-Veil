f658ae4ad1790bb0a52b94c2b51ef9549ebd098b
Joshua Loong <joshualoongwy@gmail.com>
2024-04-22 20:58:07 +1000

    feature/86cv5njry: Added ability to override styling for service details header and added service details header to examples page

--- behind-the-veil-siteroot/imports/ui/components/service-details-header/ServiceDetailsHeader.jsx
+++ behind-the-veil-siteroot/imports/ui/components/service-details-header/ServiceDetailsHeader.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import Card from "../card/Card";
+import classNames from "classnames";
 
 /**
  * Component that displays service details, i.e. service, date, artist, price
+ * @param {string} className Additional classes to be applied on top of the base style
  * @param {string} service Name of service
  * @param {string} date Date of service performed
  * @param {string} artist Name of artist providing service
  * @param {string} price Price of service
  * // TODO: may need to refactor props once the database schema is set up
  */
-const ServiceDetailsHeader = ({ service, date, artist, price }) => {
+const ServiceDetailsHeader = ({ className, service, date, artist, price }) => {
+  const classes = classNames("service-details-header-base", className)
   return (
-    <Card className="grid grid-cols-2 grid-rows-2 gap-x-10 gap-y-4 w-full">
+    <Card className={classes}>
       <div>
         <div className="main-text text-dark-grey">Service</div>
         <div className="main-text text-our-black">{service}</div>
