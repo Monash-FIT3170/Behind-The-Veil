9b6e8a54a7bf60b50355f1af5282ee65278cfa47
kbib0002 <kyleedb@icloud.com>
2024-04-26 12:21:48 +1000

    Added base components to Sign-in Page.
    Added all DOM elements into the Sign-in Page. Now, they just need to be organised.

--- behind-the-veil-siteroot/imports/ui/components/pages/login/LoginPage.jsx
+++ behind-the-veil-siteroot/imports/ui/components/pages/login/LoginPage.jsx
@@ -7,6 +7,7 @@
 import React from 'react';
 import WhiteBackground from "../../whiteBackground/WhiteBackground.jsx";
 import PageLayout from "../../../enums/PageLayout";
+import Button from "../../button/Button.jsx";
 
 /**
  * Page where user can sign in to their account
@@ -16,15 +17,24 @@ export const LoginPage = () => {
         // if window size is SMALLER than a large screen (default variable for large in tailwind lg:1024px),
         // then use center aligned and no visuals on the left so the inputs aren't all squished
         <WhiteBackground pageLayout={window.innerWidth <= 1024 ? PageLayout.SMALL_CENTER : PageLayout.SMALL_RIGHT}>
-            {/*you MUST keep this div and put everything on the left side (e.g. the visual) of it*/}
+
             <div className="hidden lg:flex translate-x-1/2 translate-y-[80vh]">
+                
                 {/*You might have to alter the above translation values or something to make sure that the visual
                 doesn't move when changing screen size*/}
-                <span>Sign In Page Visual here!!</span>
+                <div className="title-text">Bridal Makeup & Services</div>
+
             </div>
-            {/*you MUST keep this div and put everything on the right side inside of it*/}
+
             <div>
-                <span>Sign In Page to be done!!</span>
+
+                <div className="title-text">Sign In</div>
+                <input type="text" placeholder="Username"></input>
+                <input type="text" placeholder="Password"></input>
+                <div className="link-text underline">Forgot password?</div>
+                <Button className="bg-secondary-purple hover:bg-secondary-purple-hover">Sign in</Button>
+                <Button>Register</Button>
+
             </div>
         </WhiteBackground>
     );
