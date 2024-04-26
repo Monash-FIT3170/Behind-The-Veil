ee4913be5cb78733ca5720b8f5e8ec6551763926
jrsharpie <lsha0034@student.monash.edu>
2024-04-26 23:45:37 +1000

    feature/86cuvrpyf Added main elements of home page, search bar and buttons

--- behind-the-veil-siteroot/imports/ui/components/pages/home/HomePage.jsx
+++ behind-the-veil-siteroot/imports/ui/components/pages/home/HomePage.jsx
@@ -7,6 +7,8 @@
 import React from 'react';
 import WhiteBackground from "../../whiteBackground/WhiteBackground.jsx";
 import PageLayout from "../../../enums/PageLayout";
+import Button from "../../button/Button.jsx";
+import './homePage.css';
 
 /**
  * Home page which is also the landing page
@@ -14,7 +16,31 @@ import PageLayout from "../../../enums/PageLayout";
 export const HomePage = () => {
     return (
         <WhiteBackground pageLayout={PageLayout.LARGE_CENTER}>
-            <div className="search-bar"><input type="search" name="home-search" id="home-search" className='search-input' onChange={(e) => setSearchQuery(e.target.value)} placeholder="Name, Description, etc ..."></input></div>
+            <div class="container mx-auto px-20">
+                <p class="h-96">image</p>
+            </div>
+            <div class="container mx-auto px-20">
+                <form class="flex items-center justify-center space-x-3">
+                    <input class="border-2 border-gray-300 rounded h-8 w-2/5 main-text" type="search" name="home-search" id="home-search" placeholder="Name, Description, etc ..."></input>
+                    <select class="border-2 border-gray-300 rounded h-8 w-1/6 main-text">
+                        <option class="main-text" value="all" selected>All</option>
+                        <option class="main-text" value="hair">Hair</option>
+                        <option class="main-text" value="makeup">Makeup</option>
+                    </select>
+                    <Button className="bg-secondary-purple hover:bg-secondary-purple-hover rounded-full h-8 w-8">
+                    </Button>
+                </form>
+            </div>
+            <div class="container mx-auto px-20">
+                <form class="flex items-center justify-center space-x-8">
+                    <Button className="h-8 w-80 main-text">
+                        Browse Services
+                    </Button>
+                    <Button className="h-8 w-80 main-text">
+                        Browse Artists
+                    </Button>
+                </form>
+            </div>
         </WhiteBackground>
     );
 };
