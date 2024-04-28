52ed3e93b8a95e48a3e79f3f0666d8fc13dd5a36
jrsharpie <lsha0034@student.monash.edu>
2024-04-28 23:49:34 +1000

    feature/86cuvrpyf Finished most of the home page, just need icons and some feedback

--- behind-the-veil-siteroot/imports/ui/components/pages/home/HomePage.jsx
+++ behind-the-veil-siteroot/imports/ui/components/pages/home/HomePage.jsx
@@ -4,42 +4,108 @@
  * Contributors:
  */
 
-import React from 'react';
+import React, { useState } from "react";
 import WhiteBackground from "../../whiteBackground/WhiteBackground.jsx";
 import PageLayout from "../../../enums/PageLayout";
 import Button from "../../button/Button.jsx";
-import image1 from "/images/image1.jpg";
-import './homePage.css';
+import "./homePage.css";
+
+const images = [
+    "/images/pexels-zvolskiy-1721944.jpg",
+    "/images/pexels-kristina-polianskaia-2617447-4241704.jpg",
+    "/images/pexels-kseniia-lopyreva-3299160-4959838.jpg",
+];
 
 /**
  * Home page which is also the landing page
  */
 export const HomePage = () => {
-    // console.log(image1);
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    const goToPreviousImage = () => {
+        setCurrentImageIndex((prevIndex) =>
+            prevIndex === 0 ? images.length - 1 : prevIndex - 1
+        );
+    };
+
+    const goToNextImage = () => {
+        setCurrentImageIndex((prevIndex) =>
+            prevIndex === images.length - 1 ? 0 : prevIndex + 1
+        );
+    };
+
     return (
         <WhiteBackground pageLayout={PageLayout.LARGE_CENTER}>
-            <div className="container mx-auto px-20 h-96">
-                <img src={image1} />
-                {/* <img src={process.env.PUBLIC_URL} className="object-scale-down h-48 w-full"/> */}
+            <div className="inline-flex items-center justify-center h-xl">
+                <div className="flex items-start justify-center w-1/6">
+                    <Button onClick={goToPreviousImage}>Prev</Button>
+                </div>
+                <div className="inline-flex justify-center w-4/6 h-full">
+                    <div className="flex items-start justify-end h-full">
+                        <img
+                            src={
+                                images[
+                                    currentImageIndex === 0
+                                        ? images.length - 1
+                                        : currentImageIndex - 1
+                                ]
+                            }
+                            className="object-cover h-96 w-72 rounded-4xl opacity-50 -mr-16 "
+                        />
+                    </div>
+                    <div className="flex items-center h-full">
+                        <img
+                            src={images[currentImageIndex]}
+                            className="object-cover h-96 w-96 rounded-4xl z-10"
+                        />
+                    </div>
+                    <div className="flex items-start h-full">
+                        <img
+                            src={
+                                images[
+                                    currentImageIndex === images.length - 1
+                                        ? 0
+                                        : currentImageIndex + 1
+                                ]
+                            }
+                            className="object-cover h-96 w-72 rounded-4xl opacity-50 -ml-16"
+                        />
+                    </div>
+                </div>
+                <div className="flex items-start justify-center w-1/6">
+                    <Button onClick={goToNextImage}>Next</Button>
+                </div>
             </div>
+            <div className="absolute top-4/10 left-1/4 w-1/2 z-20 main-text text-white text-5xl rounded-4xl bg-gradient-to-r from-purple-400/40 to-indigo-400/40"><p className="text-center">Make Yourself Perfect</p><p className="text-center pt-4">Behind The Veil</p></div>
             <div className="container mx-auto px-20">
                 <form className="flex items-center justify-center space-x-3">
-                    <input className="border-2 border-gray-300 rounded h-8 w-2/5 main-text" type="search" name="home-search" id="home-search" placeholder="Name, Description, etc ..."></input>
+                    <input
+                        className="border-2 border-gray-300 rounded h-8 w-2/5 main-text"
+                        type="search"
+                        name="home-search"
+                        id="home-search"
+                        placeholder="Name, Description, etc ..."
+                    ></input>
                     <select className="border-2 border-gray-300 rounded h-8 w-1/6 main-text">
-                        <option className="main-text" value="all" selected>All</option>
-                        <option className="main-text" value="hair">Hair</option>
-                        <option className="main-text" value="makeup">Makeup</option>
+                        <option className="main-text" value="all" selected>
+                            All
+                        </option>
+                        <option className="main-text" value="hair">
+                            Hair
+                        </option>
+                        <option className="main-text" value="makeup">
+                            Makeup
+                        </option>
                     </select>
-                    <Button className="bg-secondary-purple hover:bg-secondary-purple-hover rounded-full h-8 w-8">
-                    </Button>
+                    <Button className="bg-secondary-purple hover:bg-secondary-purple-hover rounded-full h-8 w-8"></Button>
                 </form>
             </div>
-            <div className="container mx-auto px-20">
+            <div className="container mx-auto pt-8">
                 <form className="flex items-center justify-center space-x-8">
-                    <Button className="h-8 w-80 main-text">
+                    <Button className="h-10 w-64 main-text px-0 py-0">
                         Browse Services
                     </Button>
-                    <Button className="h-8 w-80 main-text">
+                    <Button className="h-10 w-64 main-text px-0 py-0">
                         Browse Artists
                     </Button>
                 </form>
--- behind-the-veil-siteroot/tailwind.config.js
+++ behind-the-veil-siteroot/tailwind.config.js
@@ -1,32 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./imports/ui/**/*.{js,jsx,ts,tsx}", "./client/*.html"],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'main-bkg': "url('http://localhost:8000/Background.svg')", // host image on python server
-      },
-      colors: {
-        "our-black": "#282828",
-        "dark-grey": "#757575",
-        "light-grey": "#D4D4D4",
-        "light-grey-hover": "#b2b2b2",
-        "main-blue": "#BBCAFE",
-        "main-blue-hover": "#818FF8",
-        "secondary-purple": "#BBB4E8",
-        "secondary-purple-hover": "#927ED4",
-        "hyperlink-colour": "#875EB5",
-        "anything-yellow": "#FFFDE7",
-        "confirmed-colour": "#27AD6D",
-        "pending-colour": "#4F76D9",
-        "cancelled-colour": "#D33B3B",
-        "bg-gradient-start": "#BBCAFE",
-        "bg-gradient-end": "#E5D6FF"
-      },
+    content: ["./imports/ui/**/*.{js,jsx,ts,tsx}", "./client/*.html"],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "main-bkg": "url('http://localhost:8000/Background.svg')", // host image on python server
+            },
+            colors: {
+                "our-black": "#282828",
+                "dark-grey": "#757575",
+                "light-grey": "#D4D4D4",
+                "light-grey-hover": "#b2b2b2",
+                "main-blue": "#BBCAFE",
+                "main-blue-hover": "#818FF8",
+                "secondary-purple": "#BBB4E8",
+                "secondary-purple-hover": "#927ED4",
+                "hyperlink-colour": "#875EB5",
+                "anything-yellow": "#FFFDE7",
+                "confirmed-colour": "#27AD6D",
+                "pending-colour": "#4F76D9",
+                "cancelled-colour": "#D33B3B",
+                "bg-gradient-start": "#BBCAFE",
+                "bg-gradient-end": "#E5D6FF",
+            },
+            borderRadius: {
+                "4xl": "2rem",
+            },
+            height: {
+                xl: "30rem",
+            },
+            inset: {
+                "4/10": "40%",
+            },
+        },
     },
-  },
-  plugins: [
-    require('@headlessui/tailwindcss')
-  ],
+    plugins: [require("@headlessui/tailwindcss")],
 };
