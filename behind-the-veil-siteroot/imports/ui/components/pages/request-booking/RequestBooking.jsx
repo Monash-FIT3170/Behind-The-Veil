4a7e83e5dbe7bc9b55663fb967fd70443def2237
Joshua Loong <joshualoongwy@gmail.com>
2024-04-28 11:57:07 +1000

    feature/86cv5njvp: added white background to request booking page

--- behind-the-veil-siteroot/imports/ui/components/pages/request-booking/RequestBooking.jsx
+++ behind-the-veil-siteroot/imports/ui/components/pages/request-booking/RequestBooking.jsx
@@ -6,6 +6,8 @@
 
 import React from "react";
 import ServiceDetailsHeader from "../../service-details-header/ServiceDetailsHeader";
+import WhiteBackground from "../../whiteBackground/WhiteBackground";
+import PageLayout from "../../../enums/PageLayout";
 
 /**
  * Page for user to request a booking
@@ -19,15 +21,18 @@ const RequestBooking = () => {
   }
 
   return (
-    <div className="flex flex-col gap-4 px-40 py-10">
-      <div className="large-text">Request Booking</div>
-      <ServiceDetailsHeader 
-        service={MOCK_SERVICE_DETAILS.service}
-        date={MOCK_SERVICE_DETAILS.date}
-        artist={MOCK_SERVICE_DETAILS.artist}
-        price={MOCK_SERVICE_DETAILS.price}
-      />
-    </div>
+    <WhiteBackground pageLayout={PageLayout.LARGE_CENTER}>
+      <div className="flex flex-col gap-4 px-40 py-10">
+        <div className="large-text">Request Booking</div>
+        <ServiceDetailsHeader
+          service={MOCK_SERVICE_DETAILS.service}
+          date={MOCK_SERVICE_DETAILS.date}
+          artist={MOCK_SERVICE_DETAILS.artist}
+          price={MOCK_SERVICE_DETAILS.price}
+        />
+      </div>
+    </WhiteBackground>
+
   );
 };
 
