86ae1059dd78942a879fa1d218961225a535a1a3
jrsharpie <lsha0034@student.monash.edu>
2024-04-28 20:20:06 +1000

    Uploaded the images to an image file

--- behind-the-veil-siteroot/imports/ui/components/pages/home/HomePage.jsx
+++ behind-the-veil-siteroot/imports/ui/components/pages/home/HomePage.jsx
@@ -8,31 +8,34 @@ import React from 'react';
 import WhiteBackground from "../../whiteBackground/WhiteBackground.jsx";
 import PageLayout from "../../../enums/PageLayout";
 import Button from "../../button/Button.jsx";
+import image1 from "/images/image1.jpg";
 import './homePage.css';
 
 /**
  * Home page which is also the landing page
  */
 export const HomePage = () => {
+    // console.log(image1);
     return (
         <WhiteBackground pageLayout={PageLayout.LARGE_CENTER}>
-            <div class="container mx-auto px-20">
-                <p class="h-96">image</p>
+            <div className="container mx-auto px-20 h-96">
+                <img src={image1} />
+                {/* <img src={process.env.PUBLIC_URL} className="object-scale-down h-48 w-full"/> */}
             </div>
-            <div class="container mx-auto px-20">
-                <form class="flex items-center justify-center space-x-3">
-                    <input class="border-2 border-gray-300 rounded h-8 w-2/5 main-text" type="search" name="home-search" id="home-search" placeholder="Name, Description, etc ..."></input>
-                    <select class="border-2 border-gray-300 rounded h-8 w-1/6 main-text">
-                        <option class="main-text" value="all" selected>All</option>
-                        <option class="main-text" value="hair">Hair</option>
-                        <option class="main-text" value="makeup">Makeup</option>
+            <div className="container mx-auto px-20">
+                <form className="flex items-center justify-center space-x-3">
+                    <input className="border-2 border-gray-300 rounded h-8 w-2/5 main-text" type="search" name="home-search" id="home-search" placeholder="Name, Description, etc ..."></input>
+                    <select className="border-2 border-gray-300 rounded h-8 w-1/6 main-text">
+                        <option className="main-text" value="all" selected>All</option>
+                        <option className="main-text" value="hair">Hair</option>
+                        <option className="main-text" value="makeup">Makeup</option>
                     </select>
                     <Button className="bg-secondary-purple hover:bg-secondary-purple-hover rounded-full h-8 w-8">
                     </Button>
                 </form>
             </div>
-            <div class="container mx-auto px-20">
-                <form class="flex items-center justify-center space-x-8">
+            <div className="container mx-auto px-20">
+                <form className="flex items-center justify-center space-x-8">
                     <Button className="h-8 w-80 main-text">
                         Browse Services
                     </Button>
