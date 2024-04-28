931232fb4fc5e0d695a9f6157c169f75b8732992
Joshua Loong <joshualoongwy@gmail.com>
2024-04-28 12:12:00 +1000

    feature/86cv5njvp: added back button to request booking page

--- behind-the-veil-siteroot/imports/ui/components/pages/request-booking/RequestBooking.jsx
+++ behind-the-veil-siteroot/imports/ui/components/pages/request-booking/RequestBooking.jsx
@@ -8,6 +8,8 @@ import React from "react";
 import ServiceDetailsHeader from "../../service-details-header/ServiceDetailsHeader";
 import WhiteBackground from "../../whiteBackground/WhiteBackground";
 import PageLayout from "../../../enums/PageLayout";
+import Button from "../../button/Button";
+import {ArrowLeftIcon} from '@heroicons/react/24/outline'
 
 /**
  * Page for user to request a booking
@@ -22,7 +24,12 @@ const RequestBooking = () => {
 
   return (
     <WhiteBackground pageLayout={PageLayout.LARGE_CENTER}>
-      <div className="flex flex-col gap-4 px-40 py-10">
+      {/* TODO: implement back button functionality when implementing the actual work flow to get to this page */}
+      <Button className="bg-transparent text-dark-grey flex gap-2">
+        <ArrowLeftIcon className="size-6"/>
+        Back
+      </Button>
+      <div className="flex flex-col gap-4 px-40">
         <div className="large-text">Request Booking</div>
         <ServiceDetailsHeader
           service={MOCK_SERVICE_DETAILS.service}
