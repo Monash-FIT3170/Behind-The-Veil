04665fceba622db076e64d0d95d076df063c6f45
jrsharpie <lsha0034@student.monash.edu>
2024-04-29 23:22:13 +1000

    feature/86cuvrpyf updated the responsiveness of the home page for most contents (all except for search line)

--- behind-the-veil-siteroot/imports/ui/components/pages/home/HomePage.jsx
+++ behind-the-veil-siteroot/imports/ui/components/pages/home/HomePage.jsx
@@ -45,13 +45,13 @@ export const HomePage = () => {
     return (
         <WhiteBackground pageLayout={PageLayout.LARGE_CENTER}>
             <div className="inline-flex items-center justify-center h-[30rem]">
-                <div className="flex items-start justify-center w-1/6">
+                <div className="hidden md:flex md:items-start md:justify-center md:w-1/6">
                     <Button onClick={goToPreviousImage} className="bg-white">
                         <ChevronLeftIcon className="size-10 stroke-[4]" />
                     </Button>
                 </div>
-                <div className="inline-flex justify-center w-4/6 h-full">
-                    <div className="flex items-start justify-end h-full">
+                <div className="inline-flex justify-center w-full md:w-4/6 h-full">
+                    <div className="hidden lg:flex lg:items-start lg:justify-end lg:h-full">
                         <img
                             src={
                                 images[
@@ -63,13 +63,13 @@ export const HomePage = () => {
                             className="object-cover h-96 w-72 rounded-[2rem] brightness-50 -mr-16 shadow-2xl"
                         />
                     </div>
-                    <div className="flex items-center h-full">
+                    <div className="flex md:items-center h-full">
                         <img
                             src={images[currentImageIndex]}
-                            className="object-cover h-96 w-96 rounded-[2rem] z-10 shadow-2xl"
+                            className="object-cover w-full h-96 md:w-96 rounded-[2rem] z-10 shadow-2xl"
                         />
                     </div>
-                    <div className="flex items-start h-full">
+                    <div className="hidden lg:flex lg:items-start lg:h-full">
                         <img
                             src={
                                 images[
@@ -82,18 +82,22 @@ export const HomePage = () => {
                         />
                     </div>
                 </div>
-                <div className="flex items-start justify-center w-1/6">
+                <div className="hidden md:flex md:items-start md:justify-center md:w-1/6">
                     <Button onClick={goToNextImage} className="bg-white">
                         <ChevronRightIcon className="size-10 stroke-[4]" />
                     </Button>
                 </div>
             </div>
-            <div className="absolute top-[40%] left-1/4 w-1/2 h-1/5 z-20 main-text text-white text-5xl rounded-[2rem] bg-gradient-to-r from-purple-400/40 to-indigo-400/40 backdrop-filter backdrop-blur-md">
-                <p className="text-center h-1/2 pt-4">Make Yourself Perfect</p>
-                <p className="inline-flex items-center justify-center text-center gap-x-5 h-1/2 w-full bottom-0 pb-4">
-                    <SparklesIcon className="size-14" fill="white" />
+            <div className="absolute rounded-[2rem] bg-gradient-to-r from-purple-400/40 to-indigo-400/40 backdrop-filter backdrop-blur-md z-20 main-text text-white top-[50%] text-xl  w-[95%] left-[2.5%]
+            sm:left-[20%] sm:w-[60%] 
+            md:top-[55%] md:text-2xl
+            lg:h-[15%] lg:text-3xl lg:top-[50%]
+            xl:left-[25%] xl:w-[50%] xl:h-[20%] xl:text-5xl">
+                <p className="text-center h-1/2 pt-2 lg:pt-4">Make Yourself Perfect</p>
+                <p className="inline-flex items-center justify-center text-center gap-x-5 h-1/2 w-full bottom-0 pb-2 lg:pb-4">
+                    <SparklesIcon className="size-6 md:size-8 lg:size-10 xl:size-14" fill="white" />
                     Behind The Veil
-                    <SparklesIcon className="size-14" fill="white" />
+                    <SparklesIcon className="size-6 md:size-8 lg:size-10 xl:size-14" fill="white" />
                 </p>
             </div>
             <div className="container mx-auto px-20">
@@ -118,7 +122,7 @@ export const HomePage = () => {
                 </form>
             </div>
             <div className="container mx-auto pt-8">
-                <form className="flex items-center justify-center space-x-8">
+                <form className="flex items-center justify-center flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-8">
                     <Button className="inline-flex items-center justify-center h-10 w-64 main-text px-0 py-0 gap-x-2">
                         <PaintBrushIcon className="size-5" />
                         Browse Services
