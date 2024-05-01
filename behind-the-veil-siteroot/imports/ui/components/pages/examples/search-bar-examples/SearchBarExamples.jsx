03d93a09e741bd0e57bc5703658737789b3dde61
jrsharpie <lsha0034@student.monash.edu>
2024-05-01 21:50:38 +1000

    feature/86cv4kz4v updated search bar ui

--- behind-the-veil-siteroot/imports/ui/components/pages/examples/search-bar-examples/SearchBarExamples.jsx
+++ behind-the-veil-siteroot/imports/ui/components/pages/examples/search-bar-examples/SearchBarExamples.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import SearchBar from "../../../searchBar/searchBar.jsx";
+import Button from "../../../button/Button.jsx";
+import {
+    MagnifyingGlassIcon,
+} from "@heroicons/react/24/outline";
 
 const SearchBarExamples = () => {
     return (
         <div className="flex flex-col gap-3">
             <div className="large-text underline">Search Bar:</div>
-            <SearchBar placeholderName="Basic Search Bar"></SearchBar>
+            <form className="flex space-x-3">
+                <SearchBar 
+                    formClassName="container h-8 w-[395px]" 
+                    searchClassName="border-2 border-gray-300 border-r-0 rounded rounded-r-none main-text align-top h-8 w-[361px] pl-[10px]" 
+                    resetClassName="bg-white main-text border-2 border-gray-300 border-l-0 rounded rounded-l-none h-8 w-[33px] hover:bg-gray-300 active:bg-gray-400 px-0 pb-[1px]" 
+                    placeholderName="Basic Search Bar">
+                </SearchBar>
+                <Button className="bg-secondary-purple hover:bg-secondary-purple-hover rounded-full h-8 w-8 px-1.5 py-0">
+                    <MagnifyingGlassIcon className="size-5 stroke-[3]" />
+                </Button>
+            </form>
+            
         </div>
     )
 }
--- behind-the-veil-siteroot/imports/ui/components/searchBar/searchBar.jsx
+++ behind-the-veil-siteroot/imports/ui/components/searchBar/searchBar.jsx
@@ -1,11 +1,11 @@
 import React, { forwardRef } from 'react';
+import {XMarkIcon} from "@heroicons/react/24/outline";
 import './searchBar.css'
-import classNames from 'classnames';
 
 const SearchBar = forwardRef(({formClassName, searchClassName, resetClassName, placeholderName}, ref) => {
-    const formClasses = formClassName ? classNames("", formClassName) : "container h-8 w-[395px]";
-    const searchClasses = searchClassName ? classNames("", searchClassName) : "border-2 border-gray-300 border-r-0 rounded rounded-r-none main-text align-top h-8 w-[361px] pl-[10px]";
-    const resetClasses = resetClassName ? classNames("", resetClassName) : "bg-white main-text border-2 border-gray-300 border-l-0 rounded rounded-l-none h-8 w-[33px] hover:bg-gray-300 active:bg-gray-400";
+    const formClasses = formClassName;
+    const searchClasses = searchClassName;
+    const resetClasses = resetClassName;
 
     return(
         <form className={formClasses}>
