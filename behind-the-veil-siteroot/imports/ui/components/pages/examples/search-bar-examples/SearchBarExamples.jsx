124f942ede28fc27a0655e9d081e09b0ceb797c2
jrsharpie <lsha0034@student.monash.edu>
2024-05-02 23:35:42 +1000

    feature/86cv4kz4v Finished off the format and added functionality of the basic search bar. Also added some comments to searchBar.jsx

--- behind-the-veil-siteroot/imports/ui/components/pages/examples/search-bar-examples/SearchBarExamples.jsx
+++ behind-the-veil-siteroot/imports/ui/components/pages/examples/search-bar-examples/SearchBarExamples.jsx
@@ -1,25 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 import SearchBar from "../../../searchBar/searchBar.jsx";
 import Button from "../../../button/Button.jsx";
 import {
     MagnifyingGlassIcon,
 } from "@heroicons/react/24/outline";
 
+
+
 const SearchBarExamples = () => {
+    const [inputValue, setInputValue] = useState('');
+
+  const handleButtonClick = () => {
+    // Accessing the input field value
+    console.log(inputValue);
+  };
+
+  const handleInputChange = (e) => {
+    // Updating the inputValue state when input changes
+    setInputValue(e.target.value);
+  };
+
     return (
         <div className="flex flex-col gap-3">
             <div className="large-text underline">Search Bar:</div>
-            <form className="flex space-x-3">
+            <div className="flex flex-col sm:flex-row items-center justify-center sm:justify-start gap-3">
                 <SearchBar 
-                    formClassName="container h-8 w-[395px]" 
-                    searchClassName="border-2 border-gray-300 border-r-0 rounded rounded-r-none main-text align-top h-8 w-[361px] pl-[10px]" 
-                    resetClassName="bg-white main-text border-2 border-gray-300 border-l-0 rounded rounded-l-none h-8 w-[33px] hover:bg-gray-300 active:bg-gray-400 px-0 pb-[1px]" 
+                    id="Basic"
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    formClassName="flex h-8 " 
+                    searchClassName="border-2 border-gray-300 border-r-0 rounded rounded-r-none main-text align-top h-8 w-[60vw] sm:w-[361px] pl-[10px]" 
+                    resetClassName="flex justify-center items-center bg-white main-text border-2 border-gray-300 border-l-0 rounded rounded-l-none h-8 w-[33px] hover:bg-gray-300 active:bg-gray-400" 
                     placeholderName="Basic Search Bar">
                 </SearchBar>
-                <Button className="bg-secondary-purple hover:bg-secondary-purple-hover rounded-full h-8 w-8 px-1.5 py-0">
+                <Button className="bg-secondary-purple hover:bg-secondary-purple-hover rounded-full h-8 w-8 px-1.5 py-0" onClick={handleButtonClick}>
                     <MagnifyingGlassIcon className="size-5 stroke-[3]" />
                 </Button>
-            </form>
+            </div>
             
         </div>
     )
--- behind-the-veil-siteroot/imports/ui/components/searchBar/searchBar.jsx
+++ behind-the-veil-siteroot/imports/ui/components/searchBar/searchBar.jsx
@@ -1,18 +1,64 @@
-import React, { forwardRef } from 'react';
-import {XMarkIcon} from "@heroicons/react/24/outline";
-import './searchBar.css'
+/**
+ * File Description: Search Bar React component
+ * File version: 1.0
+ * Contributors: Lucas
+ */
 
-const SearchBar = forwardRef(({formClassName, searchClassName, resetClassName, placeholderName}, ref) => {
-    const formClasses = formClassName;
-    const searchClasses = searchClassName;
-    const resetClasses = resetClassName;
+import React from "react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+import "./searchBar.css";
 
-    return(
-        <form className={formClasses}>
-            <input type="search" className={searchClasses} placeholder={placeholderName}/>
-            <input type="reset" className={resetClasses} value="x"/>
+/**
+ * General Search Bar component for all required instances of the searching in the app.
+ * Works with enter button on keyboard (Requires a separate button for users who use mouse click)
+ * @param formClassName custom classes for the form 
+ * @param searchClassName custom classes for the search input field
+ * @param resetClassName custom classes for the reset button
+ * @param placeholderName the placeholder name for the search input field (i.e. what the user will see if the input field is empty)
+ * @param value the value of the input field (whatever the customer inputs)
+ * @param onChange the function that occurs when the search input field changes
+ * @param searchBarProps encompasses all other props supplied and applies them to the search input field 
+ * 
+ */
+const SearchBar = ({
+    formClassName,
+    searchClassName,
+    resetClassName,
+    placeholderName,
+    value,
+    onChange,
+    ...searchBarProps
+}) => {
+
+    // Function that resets the input value of the search bar
+    const handleReset = () => {
+        onChange({ target: { value: '' } });
+    };
+
+    // Function that logs the value in the search bar to the console
+    const handleSubmit = (e) => {
+        e.preventDefault(); // prevents the automatic form submit which reloads the page
+        console.log(value);
+    }
+
+    return (
+        // The form which holds the search input field and the reset button
+        <form className={formClassName} onSubmit={handleSubmit}> 
+        {/* The search input field (i.e. the search bar) */}
+            <input
+                {...searchBarProps}
+                type="search"
+                className={searchClassName}
+                placeholder={placeholderName}
+                value={value}
+                onChange={onChange}
+            />
+        {/* The reset button which resets the value of the search input field to an empty string */}
+            <button type="button" className={resetClassName} onClick={handleReset}>
+                <XMarkIcon className="size-5 stroke-2"></XMarkIcon>
+            </button>
         </form>
     );
-});
+};
 
 export default SearchBar;
