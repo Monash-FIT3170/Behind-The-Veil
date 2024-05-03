1427be5a51500649842981b7b7c0d3dbd22a844f
jrsharpie <lsha0034@student.monash.edu>
2024-05-03 16:28:03 +1000

    feature/86cv4kz4v Finished search bar basic, with all comments detailing the functionality of the search bar

--- behind-the-veil-siteroot/imports/ui/components/pages/examples/search-bar-examples/SearchBarExamples.jsx
+++ behind-the-veil-siteroot/imports/ui/components/pages/examples/search-bar-examples/SearchBarExamples.jsx
@@ -1,45 +1,52 @@
-import React, {useState} from "react";
+/**
+ * File Description: Search Bar React component examples
+ * File version: 1.0
+ * Contributors: Lucas
+ */
+
+import React, { useState } from "react";
 import SearchBar from "../../../searchBar/searchBar.jsx";
 import Button from "../../../button/Button.jsx";
-import {
-    MagnifyingGlassIcon,
-} from "@heroicons/react/24/outline";
-
-
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 const SearchBarExamples = () => {
-    const [inputValue, setInputValue] = useState('');
+    // creates a state of the input value for the search bar
+    const [inputValue, setInputValue] = useState("");
+    
+    /**
+     * this function is used when the user wants to submit the value in the search bar, either by enter key or a button
+     * Alter this function  with whatever data manipulation is needed to be done with the input value
+     */ 
+    
+    const handleButtonClickOrSubmit = (e) => {
+        e.preventDefault(); // This line is important as it prevents the automatic submit for forms which reloads the page
+        console.log(inputValue);
+    };
 
-  const handleButtonClick = () => {
-    // Accessing the input field value
-    console.log(inputValue);
-  };
-
-  const handleInputChange = (e) => {
-    // Updating the inputValue state when input changes
-    setInputValue(e.target.value);
-  };
+    // this function updates the inputValue state when input changes
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
 
     return (
         <div className="flex flex-col gap-3">
             <div className="large-text underline">Search Bar:</div>
             <div className="flex flex-col sm:flex-row items-center justify-center sm:justify-start gap-3">
-                <SearchBar 
-                    id="Basic"
+                <SearchBar
                     value={inputValue}
                     onChange={handleInputChange}
-                    formClassName="flex h-8 " 
-                    searchClassName="border-2 border-gray-300 border-r-0 rounded rounded-r-none main-text align-top h-8 w-[60vw] sm:w-[361px] pl-[10px]" 
-                    resetClassName="flex justify-center items-center bg-white main-text border-2 border-gray-300 border-l-0 rounded rounded-l-none h-8 w-[33px] hover:bg-gray-300 active:bg-gray-400" 
-                    placeholderName="Basic Search Bar">
-                </SearchBar>
-                <Button className="bg-secondary-purple hover:bg-secondary-purple-hover rounded-full h-8 w-8 px-1.5 py-0" onClick={handleButtonClick}>
+                    handleSubmit={handleButtonClickOrSubmit}
+                    placeholderName="Basic Search Bar"
+                ></SearchBar>
+                <Button
+                    className="bg-secondary-purple hover:bg-secondary-purple-hover rounded-full h-8 w-8 px-1.5 py-0"
+                    onClick={handleButtonClickOrSubmit}
+                >
                     <MagnifyingGlassIcon className="size-5 stroke-[3]" />
                 </Button>
             </div>
-            
         </div>
-    )
-}
+    );
+};
 
 export default SearchBarExamples;
