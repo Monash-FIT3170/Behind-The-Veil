edbcaa17b4bba06c098de84ddbd6a1b93a271edd
kbib0002 <kyleedb@icloud.com>
2024-05-04 11:10:30 +1000

    Pull Request issues resolved.
    The Register button now is linked to the Register Page.
    
    The branch has been rebased.
    
    SVGs have been created as components and then imported.

--- behind-the-veil-siteroot/imports/ui/components/pages/login/LoginPage.jsx
+++ behind-the-veil-siteroot/imports/ui/components/pages/login/LoginPage.jsx
@@ -5,9 +5,12 @@
  */
 
 import React from 'react';
+import {NavLink} from "react-router-dom";
 import WhiteBackground from "../../whiteBackground/WhiteBackground.jsx";
 import PageLayout from "../../../enums/PageLayout";
 import Button from "../../button/Button.jsx";
+import PersonIcon from "../../svgs/PersonIcon.jsx";
+import KeyIcon from "../../svgs/KeyIcon.jsx";
 
 /**
  * Page where user can sign in to their account
@@ -32,32 +35,23 @@ export const LoginPage = () => {
 
                 </div>
 
-                <div className="title-text">Bridal Makeup & Services</div>
-
             </div>
 
             <div>
 
                 <div className="title-text" style={{textAlign: "center"}}>Sign In</div>
 
-                <div className="title-text" style={{textAlign: "center"}}>Sign In</div>
-
                 <div style={{alignItems: "center", display: "flex", flexDirection: "column", gap: "10px", marginTop: "50px"}}>
                     <div style={{position: 'relative'}}>
                         <span style={{position: 'absolute', left: '10px', top: '50%', transform: 'translateY(-50%)'}}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray">
-                            <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
-                        </svg>
+                        <PersonIcon></PersonIcon>
                         </span>
                         <input type="text" placeholder="Username" style={{height: "30px", width: "300px", paddingLeft: '40px', outline: "1px solid lightgray", borderRadius: "3px"}} />
                     </div>
 
                     <div style={{position: 'relative'}}>
                         <span style={{position: 'absolute', left: '10px', top: '50%', transform: 'translateY(-50%)'}}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray">
-                            <path d="M0 8a4 4 0 0 1 7.465-2H14a.5.5 0 0 1 .354.146l1.5 1.5a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0L13 9.207l-.646.647a.5.5 0 0 1-.708 0L11 9.207l-.646.647a.5.5 0 0 1-.708 0L9 9.207l-.646.647A.5.5 0 0 1 8 10h-.535A4 4 0 0 1 0 8m4-3a3 3 0 1 0 2.712 4.285A.5.5 0 0 1 7.163 9h.63l.853-.854a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.793-.793-1-1h-6.63a.5.5 0 0 1-.451-.285A3 3 0 0 0 4 5"/>
-                            <path d="M4 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
-                        </svg>
+                        <KeyIcon></KeyIcon>
                         </span>
                         <input type="text" placeholder="Password" style={{height: "30px", width: "300px", paddingLeft: '40px', outline: "1px solid lightgray", borderRadius: "3px"}} />
                     </div>
@@ -68,7 +62,8 @@ export const LoginPage = () => {
                 <div style={{alignItems: "center", display: "flex", flexDirection: "column", gap: "10px", marginTop: "50px"}}>
                     <Button className="bg-secondary-purple hover:bg-secondary-purple-hover" style={{width: "150px"}}>Sign in</Button>
 
-                    <Button style={{width: "150px"}}>Register</Button>
+                    <NavLink to="/register"><Button style={{width: "150px"}}>Register</Button></NavLink>
+
                 </div>
 
             </div>
