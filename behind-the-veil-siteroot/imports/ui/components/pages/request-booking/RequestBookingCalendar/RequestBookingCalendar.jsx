860ff03df2695e529258b7426b254b82f73b14ae
Joshua Loong <joshualoongwy@gmail.com>
2024-05-04 21:46:29 +1000

    feature/86cuw4hek: calendar days colour styling

--- behind-the-veil-siteroot/imports/ui/components/pages/request-booking/RequestBookingCalendar/RequestBookingCalendar.jsx
+++ behind-the-veil-siteroot/imports/ui/components/pages/request-booking/RequestBookingCalendar/RequestBookingCalendar.jsx
@@ -2,27 +2,18 @@ import React from "react";
 import Calendar from "react-calendar";
 import "./RequestBookingCalendar.css";
 import { addYears } from "date-fns";
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline';
-
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
 
 const RequestBookingCalendar = ({ ...props }) => {
   return (
     <Calendar
       minDate={new Date()} // min date = today
       maxDate={addYears(new Date(), 3)} // max date = 3 yrs from now
-
-
-      prevLabel={<ChevronLeftIcon className="size-4"/>}
+      prevLabel={<ChevronLeftIcon className="size-4" />}
       prev2Label={null}
-
-      nextLabel={<ChevronRightIcon className="size-4"/>}
+      nextLabel={<ChevronRightIcon className="size-4" />}
       next2Label={null}
-      
-      
-      
       {...props}
-
-
     />
   );
 };
