2e732df1a841531bda48eee8c5e05daf5f4cca0a
Hirun <himeshahettigoda@gmail.com>
2024-05-05 17:34:51 +1000

    feature/86cuvrg5v: artist service area page completed

--- behind-the-veil-siteroot/imports/ui/components/pages/artist/ArtistServiceArea.jsx
+++ behind-the-veil-siteroot/imports/ui/components/pages/artist/ArtistServiceArea.jsx
@@ -8,7 +8,10 @@ import WhiteBackground from "../../whiteBackground/WhiteBackground.jsx";
 import PageLayout from "../../../enums/PageLayout";
 import Tabs from "../../tabs/Tabs.jsx";
 import Button from "../../button/Button.jsx";
-import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline'
+import { ArrowLeftIcon } from "@heroicons/react/24/outline";
+import { TrashIcon } from "@heroicons/react/24/outline";
+import { PlusIcon } from "@heroicons/react/24/outline";
+import { CheckIcon } from "@heroicons/react/24/outline";
 
 /**
  * This page allows the artist to enter to enter a location as well as provide a radius in which they can travel from that location
@@ -16,29 +19,32 @@ import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline'
 
 export const ArtistServiceArea = () => {
   const ServiceAreaPanel = (
-<div className="grid grid-cols-[5fr_1fr_1fr] gap-4">
-      <div className="large-text">Location</div>
+    <div className="grid grid-cols-[1fr_auto_1fr] gap-4">{/* creates 3 columns with desired ratio */}
+      <div className="large-text">Service Location</div>
       <div className="large-text">Radius (km)</div>
       <div></div>
       {/* row 1 */}
       <div className="main-text text-dark-grey">Melbourne CBD</div>
       <div className="main-text text-dark-grey">15</div>
-      <div>TRASH ICON</div>
+      <Button className="bg-transparent">
+      <TrashIcon className="size-6" />
+      </Button>
       {/* inputs */}
-      <input
-        type="text"
-        className="rounded outline outline-1 outline-light-grey p-2l"
-      />
       <input
         type="text"
         className="rounded outline outline-1 outline-light-grey p-2"
       />
+      <input
+        type="text"
+        className="rounded outline outline-1 outline-light-grey p-2 w-32"
+      />
       {/* use plus icon instead of + */}
-      <Button>+ Add</Button>
-        <Button className="bg-secondary-purple hover:bg-secondary-purple-hover">
-          Save Changes
-        </Button>
-      </div>
+      <Button className = "flex gap-2"><PlusIcon className="size-6" /></Button>
+      <Button className="bg-secondary-purple hover:bg-secondary-purple-hover flex gap-2">
+        <CheckIcon className="size-6" />
+        Save Changes
+      </Button>
+    </div>
   );
 
   return (
@@ -49,8 +55,8 @@ export const ArtistServiceArea = () => {
       </Button>
       <div className="title-text text-center">Settings</div>
       <Tabs
-        tabs={["Payment Details", "Service Area"]}
-        tabPanels={["Tab Panel 1", ServiceAreaPanel]}
+        tabs={["Payment Details","Change Password","Service Area"]}
+        tabPanels={["Tab Panel 1","Tab Panel 2", ServiceAreaPanel]}
       />
     </WhiteBackground>
   );
