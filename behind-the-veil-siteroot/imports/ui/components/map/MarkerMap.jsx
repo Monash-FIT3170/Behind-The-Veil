4a9fefcea25e75cb1bf43f3cbbdc2cad29c6b79c
lzha0181 <lzha0181@student.monash.edu>
2024-05-06 14:18:50 +1000

    feature/86cv9zk19 edit comments

--- behind-the-veil-siteroot/imports/ui/components/map/MarkerMap.jsx
+++ behind-the-veil-siteroot/imports/ui/components/map/MarkerMap.jsx
@@ -1,5 +1,5 @@
 /**
- * File Description: A simple map component with a center at given location
+ * File Description: A simple map component with a marker at the given location
  * File version: 1.0
  * Contributors: Laura
  */
@@ -9,7 +9,7 @@ import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css'
 
 import classNames from "classnames";
-import { mapboxKey, getCoordinates } from './utils'; ``
+import { mapboxKey, getCoordinates } from './utils'; 
 
 export const MarkerMap = ({ className, location }) => {
 
@@ -21,7 +21,6 @@ export const MarkerMap = ({ className, location }) => {
     const map = useRef(null);
     const [zoom, setZoom] = useState(14);
     const australiaBounds = [[96.8168, -43.7405], [173.0205, -9.1422]];
-    const marker = useRef(null);
 
     useEffect(() => {
         if (map.current) return;
