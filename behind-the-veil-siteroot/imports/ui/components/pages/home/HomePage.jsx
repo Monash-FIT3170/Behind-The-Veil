70819590d4d40f72bda4d36792cbccf65958d341
jrsharpie <lsha0034@student.monash.edu>
2024-05-06 11:26:17 +1000

    feature/86cuvrpyf added in the search bar and made it reactive with the home page, also added routing to the browse services and browse artists buttons

--- behind-the-veil-siteroot/imports/ui/components/pages/home/HomePage.jsx
+++ behind-the-veil-siteroot/imports/ui/components/pages/home/HomePage.jsx
@@ -5,9 +5,13 @@
  */
 
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
 import WhiteBackground from "../../whiteBackground/WhiteBackground.jsx";
 import PageLayout from "../../../enums/PageLayout";
 import Button from "../../button/Button.jsx";
+import SearchBar from "../../searchBar/searchBar.jsx";
+
 import {
     ChevronRightIcon,
     ChevronLeftIcon,
@@ -30,6 +34,9 @@ const images = [
 export const HomePage = () => {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
+    // creates a state of the input value for the search bar
+    const [inputValue, setInputValue] = useState("");
+
     const goToPreviousImage = () => {
         setCurrentImageIndex((prevIndex) =>
             prevIndex === 0 ? images.length - 1 : prevIndex - 1
@@ -40,6 +47,21 @@ export const HomePage = () => {
         setCurrentImageIndex((prevIndex) =>
             prevIndex === images.length - 1 ? 0 : prevIndex + 1
         );
+    };
+
+    /**
+     * this function is used when the user wants to submit the value in the search bar, either by enter key or a button
+     * Alter this function  with whatever data manipulation is needed to be done with the input value
+     */
+
+    const handleButtonClickOrSubmit = (e) => {
+        e.preventDefault(); // This line is important as it prevents the automatic submit for forms which reloads the page
+        console.log(inputValue);
+    };
+
+    // this function updates the inputValue state when input changes
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
     };
 
     return (
@@ -100,46 +122,75 @@ export const HomePage = () => {
                     </Button>
                 </div>
             </div>
-            <div className="absolute rounded-[2rem] bg-gradient-to-r from-purple-400/40 to-indigo-400/40 backdrop-filter backdrop-blur-md z-20 main-text text-white top-[50%] text-xl  w-[95%] left-[2.5%]
-            sm:left-[20%] sm:w-[60%] 
+            <div
+                className="absolute rounded-[2rem] bg-gradient-to-r from-purple-400/40 to-indigo-400/40 backdrop-filter backdrop-blur-md z-20 main-text text-white top-[45%] text-xl  w-[95%] left-[2.5%]
+            sm:top-[50%] sm:left-[20%] sm:w-[60%] 
             md:top-[55%] md:text-2xl
             lg:h-[15%] lg:text-3xl lg:top-[50%]
-            xl:left-[25%] xl:w-[50%] xl:h-[20%] xl:text-5xl">
-                <p className="text-center h-1/2 pt-2 lg:pt-4">Make Yourself Perfect</p>
+            xl:left-[25%] xl:w-[50%] xl:h-[20%] xl:text-5xl"
+            >
+                <p className="text-center h-1/2 pt-2 lg:pt-4">
+                    Make Yourself Perfect
+                </p>
                 <p className="inline-flex items-center justify-center text-center gap-x-5 h-1/2 w-full bottom-0 pb-2 lg:pb-4">
-                    <SparklesIcon className="size-6 md:size-8 lg:size-10 xl:size-14" fill="white" />
+                    <SparklesIcon
+                        className="size-6 md:size-8 lg:size-10 xl:size-14"
+                        fill="white"
+                    />
                     Behind The Veil
-                    <SparklesIcon className="size-6 md:size-8 lg:size-10 xl:size-14" fill="white" />
+                    <SparklesIcon
+                        className="size-6 md:size-8 lg:size-10 xl:size-14"
+                        fill="white"
+                    />
                 </p>
             </div>
             <div className="container mx-auto px-20">
-                <form className="flex items-center justify-center space-x-3">
-                    <input
-                        className="border-2 border-gray-300 rounded h-8 w-2/5 main-text"
-                        type="search"
-                        name="home-search"
-                        id="home-search"
-                        placeholder="Name, Description, etc ..."
-                    ></input>
-                    <select className="border-2 border-gray-300 rounded h-8 w-1/6 main-text">
+                <div className="flex flex-col sm:flex-row items-center justify-center gap-3">
+                    <SearchBar
+                        value={inputValue}
+                        onChange={handleInputChange}
+                        handleSubmit={handleButtonClickOrSubmit}
+                        placeholderName="Name, Description, etc..."
+                    ></SearchBar>
+                    <select className="border-2 border-gray-300 rounded h-8 w-[40vw] sm:w-[200px] main-text">
                         <option value="artists">Artists</option>
                         <option value="services">Services</option>
                     </select>
-                    <Button className="bg-secondary-purple hover:bg-secondary-purple-hover rounded-full h-8 w-8 px-1.5 py-0">
+                    <Button
+                        className="bg-secondary-purple hover:bg-secondary-purple-hover rounded-full h-8 w-8 px-1.5 py-0"
+                        onClick={handleButtonClickOrSubmit}
+                    >
                         <MagnifyingGlassIcon className="size-5" />
                     </Button>
-                </form>
+                </div>
             </div>
             <div className="container mx-auto pt-8">
                 <form className="flex items-center justify-center flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-8">
-                    <Button className="inline-flex items-center justify-center h-10 w-64 main-text px-0 py-0 gap-x-2">
-                        <PaintBrushIcon className="size-5" />
-                        Browse Services
-                    </Button>
-                    <Button className="inline-flex items-center justify-center h-10 w-64 main-text px-0 py-0 gap-x-2">
-                        <IdentificationIcon className="size-5" />
-                        Browse Artists
-                    </Button>
+                    <Link to="/services">
+                        <Button
+                            className="inline-flex items-center justify-center h-10 w-64 main-text px-0 py-0 gap-x-2"
+                            onClick={() => {
+                                autoCloseMenu();
+                                setActiveTab("services");
+                            }}
+                        >
+                            <PaintBrushIcon className="size-5" />
+                            Browse Services
+                        </Button>
+                    </Link>
+
+                    <Link to="/artists">
+                        <Button
+                            className="inline-flex items-center justify-center h-10 w-64 main-text px-0 py-0 gap-x-2"
+                            onClick={() => {
+                                autoCloseMenu();
+                                setActiveTab("artists");
+                            }}
+                        >
+                            <IdentificationIcon className="size-5" />
+                            Browse Artists
+                        </Button>
+                    </Link>
                 </form>
             </div>
         </WhiteBackground>
