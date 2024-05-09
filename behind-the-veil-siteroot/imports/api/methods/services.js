9da2a127be4d2048b2a1aa824940ef28c0d627ab
Nikki Li Shao <nsha0054@student.monash.edu>
2024-05-10 00:31:32 +1000

    feature/86cuw4czn: Added backend: service, user, image entities

--- behind-the-veil-siteroot/imports/api/methods/services.js
+++ behind-the-veil-siteroot/imports/api/methods/services.js
@@ -1,9 +1,22 @@
+/**
+ * File Description: Service database entity
+ * File version: 1.0
+ * Contributors: Nikki
+ */
+
 import {Meteor} from 'meteor/meteor'
 import {ServiceCollection} from "/imports/api/collections/services";
-import {Mongo} from 'meteor/mongo'
-import { Promise } from "meteor/promise"
 
 Meteor.methods({
+    /**
+     * Adds a new service to the database. Keep in mind this is an async method
+     * @param {string} type - type of service, bridal, makeup, etc.
+     * @param {string} name - name of service
+     * @param {string} desc - full description of the service
+     * @param {number} price - total price for service
+     * @param {number} duration - how long the service will take in hours, not counting artist preparation and commute time
+     * @param {string} artistUsername - username (id) of artist providing the service
+     */
     "add_service": function (type, name, desc, price, duration, artistUsername) {
         ServiceCollection.insert(
             {
@@ -16,63 +29,6 @@ Meteor.methods({
                 "artistUsername": artistUsername
             }
         )
-    },
-    "add_temp": function () {
-        ServiceCollection.insert(
-            {
-                "serviceType": "a",
-                "serviceName": "a",
-                "serviceDesc": "a",
-                "servicePrice": "a",
-                "serviceActive": true,
-                "serviceDuration": "a",
-                "artistUsername": "a"
-            }
-        )
-    },
-    "get_service": function () {
-        return ServiceCollection.find();
-    },
-    "get_services_whole": function () {
-        let pipeline = [
-            {
-                $match: {serviceActive: true}
-            }, {
-                $lookup: {from: "users", localField: "artistUsername", foreignField: "_id", as: "artist"}
-            }, {
-                $unwind: "$artist"
-            }, {
-                $project: {
-                    serviceType: 1,
-                    serviceName: 1,
-                    serviceDesc: 1,
-                    servicePrice: 1,
-                    serviceActive: 1,
-                    serviceDuration: 1,
-                    artistUsername: 1,
-                    artistAlias: "$artist.userAlias"
-                }
-            }
-        ];
-        let options = {};
-
-        // gets artist AND the image data associated with the service
-        // if (active && !notActive) {
-        //     //     db.services.aggregate([ { $match: { serviceActive: true } }, { $lookup:{ from: "users", localField: "artistUsername", foreignField: "_id", as: "artist" } }, { $project: { serviceType: 1, serviceName: 1, serviceDesc: 1, servicePrice: 1, serviceActive: 1, serviceDuration: 1, artistUsername: 1, artistAlias: { $first: "$artist.userAlias" } } }]);
-        //
-        //
-        // } else if (notActive && !active) {
-        //
-        // } else if (active && notActive) {
-        //
-        // } else {
-        //     // wants neither active nor not active services, return nothing
-        //     return null;
-        // }
-
-        return Promise.await(
-            ServiceCollection.rawCollection().aggregate(pipeline).toArray()
-        );
     }
 })
 
