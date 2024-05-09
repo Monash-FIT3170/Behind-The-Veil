a9cb34d04ed0b9e2687c8dbccdeb7e96b3343156
Nikki Li Shao <nsha0054@student.monash.edu>
2024-05-10 00:32:27 +1000

    feature/86cuw4czn: Added pagination examples

--- behind-the-veil-siteroot/server/main.js
+++ behind-the-veil-siteroot/server/main.js
@@ -5,6 +5,15 @@ import "/imports/api/collections/services.js";
 import "/imports/api/methods/services.js";
 import "/imports/api/publications/services.js";
 
+import "/imports/api/collections/users.js";
+import "/imports/api/methods/users.js";
+import "/imports/api/publications/users.js";
+
+import "/imports/api/collections/images.js";
+import "/imports/api/methods/images.js";
+import "/imports/api/publications/images";
+
+
 // Leave for now for any methods that need to be called on start up.
 Meteor.startup(async () => {
 
