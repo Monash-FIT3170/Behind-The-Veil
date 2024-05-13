2bf4f846eb43ba6829bfd9e7d4882fdb7eacba9f
Vicky Huang <vickyhuang025@gmail.com>
2024-05-13 11:34:19 +1000

    feature/86cv684zq-messenger-inbox-component-v2: Restored changes from feature/86cv684zq-messenger-inbox-component branch into this branch

---
+++ behind-the-veil-siteroot/imports/ui/components/message/MessagesPreview.jsx
@@ -0,0 +1,32 @@
+/**
+ * File Description: Messages Preview
+ * File version: 1.0
+ * Contributors: Vicky
+ */
+import React from "react";
+import Card from "../card/Card";
+import Button from "../button/Button";
+
+const MessagesPreview = (props) => {
+    const {userName, name, recentMessage, image, date, read} = props.data;
+    return (
+        <Card className="flex flex-row gap-2 w-full h-full border-none">
+        <div className="bg-light-grey w-1/4 p-2 rounded">Img</div>
+        <div className="flex flex-col gap-1 w-3/4">
+        {read ? (
+            <div>
+                <div className="message-name-read-text">{name}</div>
+                <div className="message-read-text line-clamp-2 overflow-hidden">{recentMessage}</div>
+                </div>
+            ) : (
+                <div>
+                    <div className="message-name-unread-text">{name}</div>
+                    <div className="message-unread-text line-clamp-2 overflow-hidden">{recentMessage}</div>
+                </div>
+            )}
+            <div className="message-tag-text flex justify-end">@{userName}</div>
+            </div>
+        </Card>
+    )
+}
+export default MessagesPreview;
--- behind-the-veil-siteroot/imports/ui/components/pages/messages/MessagesPage.jsx
+++ behind-the-veil-siteroot/imports/ui/components/pages/messages/MessagesPage.jsx
@@ -8,6 +8,7 @@ import React from 'react';
 import WhiteBackground from "../../whiteBackground/WhiteBackground.jsx";
 import PageLayout from "../../../enums/PageLayout";
 import Conversation from '../../message/Conversation';
+import MessagesPreview from '../../message/MessagesPreview';
 
 /**
  * Messages page with all users they've chatted with
@@ -24,14 +25,35 @@ const user = {
     ]
 }
 
+// Dummy data for message previews
+const messagePreviews = [
+    {
+        userName: 'titan1',
+        name: 'Annie',
+        recentMessage: 'Hi, I was wondering if you were the amazing artist that he recommended?',
+        image: '',
+        date: new Date(2024, 4, 20),
+        read: true
+    },
+    {
+        userName: 'ackmn',
+        name: 'Mikasa',
+        recentMessage: 'Hi Alice, I was wondering if you have experience with bridesmaid makeup?',
+        image: '',
+        date: new Date(2024, 3, 15),
+        read: false
+    }
+]
+
 export const MessagesPage = () => {
+    const messagePreviewsComponents = messagePreviews.map((messagePreview, index) => <MessagesPreview key = {index} data = {messagePreview}></MessagesPreview>);
     return (
         // if window size is SMALLER than a medium screen (default variable for medium in tailwind sm:768px),
         // then have the contacts/list of people on separate screens than the conversation
         <WhiteBackground pageLayout={window.innerWidth <= 768 ? PageLayout.SMALL_CENTER : PageLayout.MESSAGES_PAGE}>
             {/*you MUST keep this div and put everything on the left side inside of it*/}
-            <div>
-                <span>Messages Left Page to be done!!</span>
+            <div className="flex flex-col gap-3">
+                {messagePreviewsComponents}
             </div>
             {/*you MUST keep this div and put everything on the right side inside of it*/}
             <div>
