ca09738152609615f483a1633e0ec00e24c50ca3
lzha0181 <lzha0181@student.monash.edu>
2024-05-13 23:36:20 +1000

    feature/86cuyhxaa add booking cards

--- behind-the-veil-siteroot/imports/ui/components/booking/BookingListView.jsx
+++ behind-the-veil-siteroot/imports/ui/components/booking/BookingListView.jsx
@@ -11,18 +11,33 @@ import {useNavigate} from "react-router-dom";
 import { BookingStatus } from '../../enums/BookingStatus';
 import { BookingFilter } from '../booking/BookingsFiltersEnum'
 import Button from '../button/Button';
+import BookingCard from '../card/BookingCard';
 
 const BookingListView = () => {
-    const MOCK_BOOKING_DETAILS = {
-      service: "Bachelorette Glam Experience",
-      date: "Tuesday, 12 May, 2024",
-      artist: "Alice Tran",
-      price: "$120"
-    }
-    
+
     //filters
     const availableFilters = Object.values(BookingFilter);
     const [selectedFilter, setSelectedFilter] = useState(BookingFilter.ALL);
+
+    const MOCK_BOOKINGS = [
+        {
+            bookingId: "1",
+            serviceName: "Bachelorette Glam Experience",
+            serviceDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis vulputate erat, tristique ultrices orci. Duis fringilla mollis sapien, eu condimentum nibh pharetra quis. In ultricies mauris vitae velit commodo congue. Donec placerat elit et ullamcorper laoreet. Morbi at bibendum quam. Nunc eu elit at ipsum vehicula  a.\n",
+            servicePrice: 123,
+            serviceImageData: "",
+            bookingStartDateTime: "Tuesday, 12 May, 2024",
+            bookingStatus: BookingStatus.CONFIRMED
+        },
+        {
+            bookingId: "2",
+            serviceName: "GlamourGlow Beauty",
+            serviceDesc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis vulputate erat, tristique ultrices orci. Duis fringilla mollis sapien, eu condimentum nibh pharetra quis. In ultricies mauris vitae velit commodo congue. Donec placerat elit et ullamcorper laoreet. Morbi at bibendum quam. Nunc eu elit at ipsum vehicula  a.\n",
+            servicePrice: 123,
+            serviceImageData: "",
+            bookingStartDateTime: "Thursday, 14 May, 2024",
+            bookingStatus: BookingStatus.CONFIRMED
+        }]
 
     return (
         <div className="mt-2">
@@ -42,8 +57,23 @@ const BookingListView = () => {
                 );
                 })}
             </div>
-        <div>
-
+        <div className="mt-6">
+            <div className="flex flex-col">
+                {MOCK_BOOKINGS.map((booking) => {
+                return (
+                    <BookingCard
+                        className="mb-4"
+                        bookingId={booking.bookingId}
+                        serviceName={booking.serviceName}
+                        serviceDesc={booking.serviceDesc}
+                        servicePrice={booking.servicePrice}
+                        servicePhotoData={""}
+                        bookingStartDateTime={booking.bookingStartDateTime}
+                        bookingStatus={booking.bookingStatus}
+                    ></BookingCard>
+                );
+                })}
+            </div>
         </div>
         </div>
 
