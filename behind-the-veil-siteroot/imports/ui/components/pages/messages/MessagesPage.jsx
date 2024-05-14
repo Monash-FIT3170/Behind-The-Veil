d1b20b02eb73f41ab43fb10280a80641157a47ee
Vicky Huang <vickyhuang025@gmail.com>
2024-05-15 00:01:16 +1000

    feature/86cv686va-combine-message-conversation-and-inbox-components: Modified the dummy data so that the message preview and conversation components use the same dummy data. Modified message preview to use that dummy data

--- behind-the-veil-siteroot/imports/ui/components/pages/messages/MessagesPage.jsx
+++ behind-the-veil-siteroot/imports/ui/components/pages/messages/MessagesPage.jsx
@@ -14,50 +14,43 @@ import MessagesPreview from '../../message/MessagesPreview';
  * Messages page with all users they've chatted with
  */
 
-const user = {
-    name: "Annie",
-    profilePic: "url/to/profile/pic",
-    messages: [
-        { text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem ducimus perferendis velit placeat expedita blanditiis est, quae officiis aperiam excepturi quis architecto ullam earum dolore unde incidunt corporis qui amet!", sender: "other" },
-        { text: "Hello", sender: "other" },
-        { text: "Hi", sender: "me" },
-        { text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem ducimus perferendis velit placeat expedita blanditiis est, quae officiis aperiam excepturi quis architecto ullam earum dolore unde incidunt corporis qui amet!", sender: "me" },
-    ]
-}
-
-// Dummy data for message previews
-const messagePreviews = [
+const users = [
     {
-        userName: 'titan1',
-        name: 'Annie',
-        recentMessage: 'Hi, I was wondering if you were the amazing artist that he recommended?',
-        image: 'url/to/profile/pic',
-        date: new Date(2024, 4, 20),
-        read: true
+        name: "Annie",
+        profilePic: "url/to/profile/pic",
+        messages: [
+            { text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem ducimus perferendis velit placeat expedita blanditiis est, quae officiis aperiam excepturi quis architecto ullam earum dolore unde incidunt corporis qui amet!", sender: "other", read: true},
+            { text: "Hello", sender: "other", read: true },
+            { text: "Hi", sender: "me", read: true },
+            { text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem ducimus perferendis velit placeat expedita blanditiis est, quae officiis aperiam excepturi quis architecto ullam earum dolore unde incidunt corporis qui amet!", sender: "me", read: true },
+            { text: "Hi, I was wondering if you were the amazing artist that he recommended?", sender: "other", read: true },
+
+        ],
+        userName: 'titan1'
     },
     {
-        userName: 'ackmn',
-        name: 'Mikasa',
-        recentMessage: 'Hi Alice, I was wondering if you have experience with bridesmaid makeup?',
-        image: 'url/to/profile/pic',
-        date: new Date(2024, 3, 15),
-        read: false
-    }
+        name: "Mikasa",
+        profilePic: "url/to/profile/pic",
+        messages: [
+            { text: "Hi Alice, I was wondering if you have experience with bridesmaid makeup?", sender: "other", read: false}
+        ],
+        userName: 'ackmn'
+    },
 ]
 
 export const MessagesPage = () => {
-    const messagePreviewsComponents = messagePreviews.map((messagePreview, index) => <MessagesPreview key = {index} data = {messagePreview}></MessagesPreview>);
+    const newMsgPreviewsComponents = users.map((user, index) => <MessagesPreview key = {index} data = {user}></MessagesPreview>);
     return (
         // if window size is SMALLER than a medium screen (default variable for medium in tailwind sm:768px),
         // then have the contacts/list of people on separate screens than the conversation
         <WhiteBackground pageLayout={window.innerWidth <= 768 ? PageLayout.SMALL_CENTER : PageLayout.MESSAGES_PAGE}>
             {/*you MUST keep this div and put everything on the left side inside of it*/}
             <div className="flex flex-col gap-3">
-                {messagePreviewsComponents}
+                {newMsgPreviewsComponents}
             </div>
             {/*you MUST keep this div and put everything on the right side inside of it*/}
             <div>
-                <Conversation user={user}/>
+                <Conversation user={users[0]}/>
             </div>
         </WhiteBackground>
     );
