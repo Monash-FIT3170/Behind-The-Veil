bf4d9266d95c20d8905c5568d4bc2d585bf95da8
Joshua Loong <95511987+joshualoongwy@users.noreply.github.com>
2024-05-16 17:34:28 +1000

    Feature/86cuw4hek Request Booking Calendar Input (#48)
    * feature/86cuw4hek: install react-calendar package
    
    * feature/86cuw4hek: created separate calendar component
    
    * feature/86cuw4hek: install date fns + add min/max dates and left right chevrons to calendar
    
    * feature/86cuw4hek: styling calendar arrow buttons
    
    * feature/86cuw4hek: calendar days colour styling
    
    * feature/86cv5njvp: selected date purple circle styling
    
    * feature/86cuw4hek: slight style change for 'next step' button
    
    * feature/86cuw4hek: change booking end time field to duration
    
    * feature/86cuw4hek: created mock booking data for request booking page
    
    * feature/86cuw4hek: logic for available times
    
    * feature/86cuw4hek: available time slot buttons work fully dynamically
    
    * feature/86cuw4hek: cleanup + calendar height fit content
    
    * feature/86cuw4hek: added grey circle styling for available days
    
    * feature/86cuw4hek: date input and calendar fully functional
    
    * feature/86cuw4hek: add no available times messaging
    
    * feature/86cuw4hek: mobile responsive design
    
    * feature/86cuw4hek: init selected date on first available day
    
    * feature/86cuw4hek: cleanup css
    
    * feature/86cuw4hek: inputs carry over to booking summary page
    
    * feature/86cuw4hek: documentation
    
    * feature/86cuw4hek: RequestBookingCalendar file documentation

--- behind-the-veil-siteroot/imports/ui/components/pages/request-booking/BookingSummary.jsx
+++ behind-the-veil-siteroot/imports/ui/components/pages/request-booking/BookingSummary.jsx
@@ -1,7 +1,7 @@
 /**
  * File Description: Review the booking details summary page
- * File version: 1.0
- * Contributors: Neth
+ * File version: 1.1
+ * Contributors: Neth, Josh
  */
 
 import React from "react";
@@ -10,8 +10,7 @@ import Button from "../../button/Button";
 import { useNavigate } from "react-router-dom";
 import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/24/outline";
 import FormOutput from "./FormOutput";
-// Array of days for date formatting
-const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+import { addHours, format, enAU } from "date-fns";
 
 /**
  * Component for displaying booking summary and allowing continuation to the next step.
@@ -19,32 +18,24 @@ const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 const BookingSummary = () => {
     const tipText = "Full deposit for a service is required. If the booking is cancelled or rejected, the full fee will be refunded.";
     /**
-     * Function to calculate start and end dates based on input date, time, and service duration.
-     * @param {string} dateString - Date string in format 'YYYY-MM-DD'.
-     * @param {string} timeString - Time string in format 'HH:MM'.
+     * Function to calculate start and end dates based on input dateTime and service duration.
+     * @param {Date} startDateTime - Date object of the starting time of the booking
      * @param {number} serviceDuration - Duration of service in hours.
      * @returns {Array<Date>} - Array containing start date, end date, and formatted date range string.
      */
-    const getStartAndEndDate = (dateString, timeString, serviceDuration) => {
-        // Create start date from input date and time
-        let dateTimeStr = dateString + " " + timeString;
-        let startDate = new Date(dateTimeStr);
-        // Calculate end date by adding service duration to start date
-        let endDate = new Date(startDate.getTime() + serviceDuration * 60 * 60 * 1000);
+    const getStartAndEndDate = (startDateTime, serviceDuration) => {
+        const endDateTime = addHours(startDateTime, serviceDuration)
 
-        // Options for date formatting
-        const options = {
-            hour: 'numeric',
-            minute: '2-digit',
-            hour12: true
-        };
-        // Get day of the week from start date
-        const day = days[startDate.getDay()];
-        // Format start and end dates
-        const startFormatted = startDate.toLocaleString('en-AU', options);
-        const endFormatted = endDate.toLocaleString('en-AU', options);
+        // format day in the form of: "Thu 21/03/23"
+        const dayString = format(startDateTime, 'E P', { locale: enAU })
 
-        return [startDate, endDate, `${day} ${dateString} ${startFormatted} - ${endFormatted}`];
+        // format time interval in the form of: "10:00 AM - 12:00 PM"
+        const timeIntervalString = `${format(startDateTime, 'p', { locale: enAU })} - ${format(endDateTime, 'p', { locale: enAU })}`
+
+        // formatted string is in the form of: "Thu 21/03/23 10:00 AM - 12:00 PM"
+        const formattedString = `${dayString} ${timeIntervalString}`
+
+        return [startDateTime, endDateTime, formattedString]
     };
 
     /**
@@ -57,8 +48,7 @@ const BookingSummary = () => {
 
         // Extract location, date, and time from URL parameters
         const location = urlParams.get('location');
-        const date = urlParams.get('date');
-        const time = urlParams.get('time');
+        const dateTime = urlParams.get('time');
 
         // TODO: bride and artist details should be queried from database
         return {
@@ -66,7 +56,7 @@ const BookingSummary = () => {
             'Artist Name': 'Alice Tran',
             'Service': 'Bachelorette Glam Experience',
             'Location': location,
-            'Date': getStartAndEndDate(date, time, 2), // Hardcoded to be 2-hour duration, can be dynamic
+            'Date': getStartAndEndDate(dateTime, 2), // Hardcoded to be 2-hour duration, can be dynamic
             'Total Price': '$120.00',
         };
     };
@@ -89,7 +79,7 @@ const BookingSummary = () => {
         <WhiteBackground>
             {/* Back button */}
             <Button className="bg-transparent text-dark-grey flex gap-2" onClick={() => navigateTo('/request-booking')}>
-                <ArrowLeftIcon className="size-6"/>
+                <ArrowLeftIcon className="size-6" />
                 Back
             </Button>
             {/* Booking summary */}
@@ -104,12 +94,12 @@ const BookingSummary = () => {
                     return <FormOutput key={index} textColor="text-dark-grey" haveHelpText={false} label={key} input={value}></FormOutput>;
                 })}
                 {/* Display deposit required */}
-                <FormOutput className='deposit-input' key={queryData().length} label='Deposit Required' input='$60.00' textColor="text-cancelled-colour" haveHelpText={true} tipText={tipText}/>
+                <FormOutput className='deposit-input' key={queryData().length} label='Deposit Required' input='$60.00' textColor="text-cancelled-colour" haveHelpText={true} tipText={tipText} />
                 <br />
                 {/* Continue button */}
                 <Button className="bg-secondary-purple hover:bg-secondary-purple-hover flex gap-2" type="submit" onClick={handleSubmit}>
                     Continue
-                    <ArrowRightIcon className="size-6"/>
+                    <ArrowRightIcon className="size-6" />
                 </Button>
             </div>
         </WhiteBackground>
--- behind-the-veil-siteroot/imports/ui/components/pages/request-booking/RequestBooking.jsx
+++ behind-the-veil-siteroot/imports/ui/components/pages/request-booking/RequestBooking.jsx
@@ -1,6 +1,6 @@
 /**
  * File Description: Request Booking page
- * File version: 1.1
+ * File version: 1.2
  * Contributors: Josh, Nikki
  */
 
@@ -9,10 +9,14 @@ import ServiceDetailsHeader from "../../service-details-header/ServiceDetailsHea
 import WhiteBackground from "../../whiteBackground/WhiteBackground";
 import PageLayout from "../../../enums/PageLayout";
 import Button from "../../button/Button";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import RequestBookingCalendar, { VALID_INTERVAL } from "./RequestBookingCalendar/RequestBookingCalendar.jsx";
 import Input from "../../input/Input";
 import PreviousButton from "../../button/PreviousButton";
-import { ArrowRightIcon } from '@heroicons/react/24/outline'
-import {useNavigate} from "react-router-dom";
+import mockBookings from './mockBookings.json'
+import { addHours, areIntervalsOverlapping, eachHourOfInterval, isEqual, set, format, isValid, isDate, parse, isWithinInterval, startOfDay, addYears, startOfHour, isAfter, eachDayOfInterval, nextDay, isSameDay, addDays } from "date-fns";
+import { BookingStatus } from "../../../enums/BookingStatus.ts";
+import { useNavigate } from "react-router-dom";
 
 /**
  * Page for user to request a booking
@@ -22,32 +26,108 @@ const RequestBooking = () => {
 
   const MOCK_SERVICE_DETAILS = {
     service: "Bachelorette Glam Experience",
-    date: "Tuesday, 12 May, 2024",
     artist: "Alice Tran",
-    price: "$120"
+    price: "$120",
+  };
+
+  /**
+   * Calculate available times that the user can select, based on date, duration, and existing bookings
+   * @param {Date} date day at which we want the available time slots
+   * @param {number} duration integer corresponding to service duration in hours
+   * @param {Array} bookings array of booking objects
+   * @returns array of date objects that correspond to available times, on the hour
+   */
+  const getAvailableTimes = ({ date, duration, bookings }) => {
+    if (!(isValid(date) && isDate(date))) {
+      console.warn('invalid date')
+      return []
+    }
+    if (!Array.isArray(bookings)) {
+      console.warn('bookings is not an array')
+      return []
+    }
+
+    const hours = eachHourOfInterval({
+      // TODO: for now, assume that artists can work 6am to 7pm every day
+      start: set(date, { hours: 6, minutes: 0, seconds: 0 }),
+      end: set(date, { hours: (19 - duration), minutes: 0, seconds: 0 })
+    })
+
+    const confirmedBookings = bookings.filter((booking) => {
+      return booking.bookingStatus === BookingStatus.CONFIRMED
+    })
+
+    // for each hour, check if that hour is 'free'
+    // an hour is 'available' if the start of the hour + service duration doesn't overlap with any existing confirmed booking
+    const availableTimes = hours.filter((hour) => {
+      if (!isAfter(startOfHour(hour), new Date())) return false
+
+      // check every booking and see if they overlap with this hour + service duration
+      // if there are no overlapping bookings, then this hour is available
+      return !confirmedBookings.some((booking) => {
+        const confirmedBookingStart = booking.bookingStartDateTime
+        const confirmedBookingEnd = addHours(confirmedBookingStart, booking.bookingDuration)
+
+        return areIntervalsOverlapping(
+          { start: hour, end: addHours(hour, duration) }, // time slot interval
+          { start: confirmedBookingStart, end: confirmedBookingEnd } // confirmed booking interval
+        )
+      })
+    })
+
+    return availableTimes
+  };
+
+  // TODO: this function might not be needed once we use real bookings b/c I think start time is stored as date object
+  // converting json datetimes to js datetimes
+  const parseBookings = (bookings) => {
+    return bookings.map((booking) => {
+      return {
+        ...booking,
+        bookingStartDateTime: new Date(booking.bookingStartDateTime)
+      }
+    })
+  }
+
+  // TODO: make actual database call to get all bookings for this artist id
+  const bookings = parseBookings(mockBookings)
+
+  // TODO: get actual duration from this service
+  const duration = 2
+
+  // initialise date input to the first day with availabilities
+  const initDateInput = () => {
+    // starting with today, iterate until we find a day with available times
+    let day = startOfDay(new Date())
+    while (getAvailableTimes({ date: day, duration: duration, bookings: bookings }).length === 0) {
+      day = startOfDay(addDays(day, 1))
+      if (isAfter(day, VALID_INTERVAL.end)) return '' // beyond valid interval
+    }
+
+    return day
   }
 
   // form input values
   const [inputs, setInputs] = useState({
     location: "",
-    date: "",
-    time: ""
-  })
+    date: initDateInput(),
+    time: "",
+  });
 
   // id's
-  const locationInputId = useId()
-  const dateInputId = useId()
-  const timeInputId = useId()
+  const locationInputId = useId();
+  const dateInputId = useId();
+  const timeInputId = useId();
 
   // form related functions
   const handleInputChange = (event) => {
     const name = event.target.name;
     const value = event.target.value;
-    setInputs(i => ({ ...i, [name]: value }))
-  }
+    setInputs((i) => ({ ...i, [name]: value }));
+  };
 
   const handleSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     // TODO: implement validation, i.e. must have a valid time
 
     // pass the data to the next page via the url
@@ -55,19 +135,42 @@ const RequestBooking = () => {
     navigateTo(`/booking-summary?${query}`);
   }
 
-  // calculate available times that the user can select, based on a date
-  // TODO: implement this properly instead of returning dummy data
-  const getAvailableTimes = (date) => {
-    const AVAILABLE_TIMES = ["10:00 AM", "11:00 AM", "12:00 PM", "2:00 PM", "4:00 PM"]
-    return date !== "" ? AVAILABLE_TIMES : null
+  const handleManualDateInput = (event) => {
+    const dateInput = event.target.value;
+
+    // if manual input is a valid date within the allowable interval
+    // convert it to a date object and store it
+    const dateFormat = /^(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-(19|20)\d{2}$/
+    if (dateFormat.test(dateInput)) {
+      const parsedDate = parse(dateInput, 'dd-MM-yyyy', new Date())
+      if (
+        isValid(parsedDate) &&
+        isWithinInterval(parsedDate, VALID_INTERVAL)
+      ) {
+        setInputs((i) => ({ ...i, date: parsedDate }));
+        return
+      }
+    }
+
+    // else, just update date input with the raw value
+    setInputs((i) => ({ ...i, date: dateInput }));
+    return
   }
 
-  const availableTimes = getAvailableTimes(inputs.date)
+  // for representing date object as DD-MM-YYYY in the date input
+  const formatDateInput = (dateInput) => {
+    // if the input is a valid date and a date object, format it 
+    if (isValid(dateInput) && isDate(dateInput)) return format(dateInput, 'dd-MM-yyyy')
+
+    return dateInput
+  }
+
+  const availableTimes = getAvailableTimes({ date: inputs.date, duration: duration, bookings: bookings });
 
   return (
     <WhiteBackground pageLayout={PageLayout.LARGE_CENTER}>
       {/* TODO: implement back button functionality when implementing the actual work flow to get to this page */}
-      <PreviousButton/>
+      <PreviousButton />
 
       {/* Main container for content */}
       <div className="flex flex-col gap-4 xl:px-40">
@@ -82,83 +185,108 @@ const RequestBooking = () => {
         {/* input form */}
         <form onSubmit={handleSubmit}>
           <div className="flex flex-col gap-4">
-
             {/* location */}
-              <Input
-                id={locationInputId}
-                label={<label htmlFor={locationInputId} className="main-text text-our-black">Location</label>}
-                placeholder="Input location for service: wedding venue, address, ..."
-                name="location"
-                value={inputs.location || ""}
-                onChange={handleInputChange}
-              />
+            <Input
+              id={locationInputId}
+              label={<label htmlFor={locationInputId} className="main-text text-our-black">Location</label>}
+              placeholder="Input location for service: wedding venue, address, ..."
+              name="location"
+              value={inputs.location || ""}
+              onChange={handleInputChange}
+            />
 
             {/* date/time */}
-            <div className="flex gap-10">
-              {/* input fields */}
-              <div className="flex flex-col gap-4 grow">
+            <div className="flex flex-col md:flex-row gap-4 md:gap-10">
+              {/* date input + calendar */}
+              <div className="flex flex-col flex-grow gap-1 md:max-w-[350px] lg:max-w-[420px] xl:lg:max-w-[490px]">
+                <div className="flex flex-col gap-4">
                   <Input
                     id={dateInputId}
                     label={<label htmlFor={dateInputId} className="main-text text-our-black">Select Date</label>}
-                    placeholder="Select a date"
+                    placeholder="DD-MM-YYYY"
                     name="date"
-                    value={inputs.date || ""}
-                    onChange={handleInputChange}
+                    value={formatDateInput(inputs.date) || ""}
+                    onChange={handleManualDateInput}
                   />
-                {/* if there are available times, render the time input buttons */}
-                {
-                  availableTimes && (
-                    <div className="flex flex-col gap-1">
-                      <label htmlFor={timeInputId} className="main-text text-our-black">Select Start Time</label>
-                      <div id={timeInputId} className="grid grid-cols-2 gap-2">
-                        {
-                          availableTimes.map((time) => {
-                            const baseStyle = "w-full"
-                            const activeStyle = "bg-dark-grey text-white"
-                            const className = inputs.time === time ?
-                              `${baseStyle} ${activeStyle}` :
-                              baseStyle
+                  {/* calendar component */}
+                  <RequestBookingCalendar
+                    value={isValid(inputs.date) && isDate(inputs.date) ? inputs.date : null}
+                    onChange={(date) => {
+                      setInputs((i) => {
+                        return {
+                          ...i,
+                          date: date,
+                        };
+                      });
+                    }}
+                    tileClassName={({ date, view }) => {
+                      const availableTimes = getAvailableTimes({ date: new Date(date), duration: duration, bookings: bookings })
+                      if (
+                        view === 'month' &&
+                        availableTimes.length > 0
+                      ) {
+                        return 'available'
+                      }
+                    }}
+                  />
+                </div>
+              </div>
 
-                            return (
-                              <Button
-                                key={time}
-                                className={className}
-                                onClick={() => {
-                                  setInputs((i) => {
-                                    return {
-                                      ...i,
-                                      time: time
-                                    }
-                                  })
-                                }}
-                              >
-                                {time}
-                              </Button>
-                            )
-                          })
-                        }
-                      </div>
-                    </div>
-                  )
-                }
-
-                <Button
-                  className="bg-secondary-purple hover:bg-secondary-purple-hover flex gap-2"
-                  type="submit"
+              {/* available time buttons */}
+              {/* <div className="flex sm:flex-grow sm:justify-center"> */}
+              <div className="flex flex-col flex-grow gap-1">
+                <label
+                  htmlFor={timeInputId}
+                  className="main-text text-our-black"
                 >
-                  Next Step
-                  <ArrowRightIcon className="icon-base"/>
-                </Button>
-              </div>
+                  Select Start Time (Duration: {duration}hr)
+                </label>
+                <div id={timeInputId} className="grid grid-cols-2 gap-2">
 
-              {/* calendar component */}
-              <div className="flex grow bg-light-grey justify-center items-center text-center">
-                CALENDAR PLACEHOLDER
+                  {/* if there are available times, render the time input buttons */}
+                  {!Array.isArray(availableTimes) || availableTimes.length === 0 ?
+                    "No available times" :
+                    availableTimes.map((time) => {
+                      const baseStyle = "w-full";
+                      const activeStyle = "bg-dark-grey text-white";
+                      const className =
+                        isEqual(inputs.time, time)
+                          ? `${baseStyle} ${activeStyle}`
+                          : baseStyle;
+
+                      return (
+                        <Button
+                          key={time}
+                          className={className}
+                          onClick={() => {
+                            setInputs((i) => {
+                              return {
+                                ...i,
+                                time: time,
+                              };
+                            });
+                          }}
+                        >
+                          {format(time, 'p')}
+                        </Button>
+                      );
+                    })}
+                </div>
               </div>
+              {/* </div> */}
+
             </div>
+
+            <Button
+              className="bg-secondary-purple hover:bg-secondary-purple-hover flex gap-2 w-fit justify-center"
+              type="submit"
+            >
+              Next Step
+              <ArrowRightIcon className="icon-base" />
+            </Button>
           </div>
-        </form >
-      </div >
+        </form>
+      </div>
     </WhiteBackground>
   );
 };
