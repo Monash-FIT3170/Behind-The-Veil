6c2a822da87f6c8913da5b13a55c1a486054100a
Nikki Li Shao <nsha0054@student.monash.edu>
2024-05-18 15:56:12 +1000

    feature/86cvdmb26: Specific services page restructure: data reactive and mobile friendly

--- behind-the-veil-siteroot/imports/api/publications/services.js
+++ behind-the-veil-siteroot/imports/api/publications/services.js
@@ -17,3 +17,15 @@ Meteor.publish("active_services", function () {
 Meteor.publish("all_services", function () {
     return ServiceCollection.find();
 });
+
+
+/**
+ * Publishes one services based on given service ID to the client.
+ * This publication filters bookings based on the provided service ID
+ * @param {string} serviceId - The ID of the service to be published.
+ * @returns {Mongo.Cursor} - A cursor representing the result the service to be published.
+ */
+Meteor.publish('specific_service', function(serviceId) {
+    // Check if the username matches either the user
+    return ServiceCollection.find({_id: serviceId});
+});
--- behind-the-veil-siteroot/imports/api/publications/users.js
+++ behind-the-veil-siteroot/imports/api/publications/users.js
@@ -20,3 +20,14 @@ Meteor.publish('all_users', function() {
 Meteor.publish('all_artists', function() {
     return UserCollection.find({"profile.type":"artist"});
 });
+
+/**
+ * Publishes all bookings associated with a specific user to the client.
+ *
+ * @param {string} username - The username of the user to be published.
+ * @returns {Mongo.Cursor} - A cursor representing the result of the user to be published.
+ */
+Meteor.publish('specific_user', function(username) {
+    // Check if the username matches either the user
+    return UserCollection.find({username:username});
+});
