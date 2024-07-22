4c81521a9ffd63f231bea108db6439cf107b0767
Joshua Loong <joshualoongwy@gmail.com>
2024-07-22 17:58:19 +1000

    feature/86cv9r9vt: added autoplacement and floating triangle to popup

--- behind-the-veil-siteroot/imports/ui/components/pages/examples/popup-examples/PopupExamples.jsx
+++ behind-the-veil-siteroot/imports/ui/components/pages/examples/popup-examples/PopupExamples.jsx
@@ -4,16 +4,22 @@
  * Contributors: Josh
  */
 
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Button from "../../../button/Button";
-import { useFloating, useInteractions, useClick } from '@floating-ui/react';
+import { useFloating, useInteractions, useClick, offset, autoPlacement, arrow, FloatingArrow } from '@floating-ui/react';
 
 const PopupExamples = () => {
     const [isOpen, setIsOpen] = useState(false)
+    const arrowRef = useRef()
 
     const { refs, floatingStyles, context } = useFloating({
         open: isOpen,
-        onOpenChange: setIsOpen
+        onOpenChange: setIsOpen,
+        middleware: [
+            offset(10),
+            autoPlacement(),
+            arrow({ element: arrowRef })
+        ],
     })
 
     const click = useClick(context)
@@ -29,14 +35,24 @@ const PopupExamples = () => {
                 ref={refs.setReference}
                 {...getReferenceProps()}
             >
-                Click for popup
+                Click for auto-placed popup
             </Button>
             {isOpen && (
                 <div
                     ref={refs.setFloating}
+                    className="border border-dark-grey rounded p-4 bg-white"
                     style={floatingStyles}
                     {...getFloatingProps()}
                 >
+                    <FloatingArrow
+                        ref={arrowRef}
+                        context={context}
+                        className="
+                        fill-white 
+                        [&>path:first-of-type]:stroke-dark-grey
+                        [&>path:last-of-type]:stroke-dark-grey
+                        "
+                    />
                     Floating element
                 </div>
             )}
