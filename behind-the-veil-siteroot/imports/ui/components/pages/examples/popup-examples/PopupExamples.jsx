0f6a86354fbfb0882022272147fd1a45917e4f57
Joshua Loong <joshualoongwy@gmail.com>
2024-07-25 22:45:30 +1000

    feature/86cv9r9vt: added example for having one popup shared among a set of reference elements

--- behind-the-veil-siteroot/imports/ui/components/pages/examples/popup-examples/PopupExamples.jsx
+++ behind-the-veil-siteroot/imports/ui/components/pages/examples/popup-examples/PopupExamples.jsx
@@ -4,58 +4,16 @@
  * Contributors: Josh
  */
 
-import React, { useState, useRef } from "react";
-import Button from "../../../button/Button";
-import { useFloating, useInteractions, useClick, offset, autoPlacement, arrow, FloatingArrow } from '@floating-ui/react';
+import React from "react";
+import SinglePopupForSetExample from "./SinglePopupForSetExample";
+import SimpleAutoplacedExample from "./SimpleAutoplacedExample";
 
 const PopupExamples = () => {
-    const [isOpen, setIsOpen] = useState(false)
-    const arrowRef = useRef()
-
-    const { refs, floatingStyles, context } = useFloating({
-        open: isOpen,
-        onOpenChange: setIsOpen,
-        middleware: [
-            offset(10),
-            autoPlacement(),
-            arrow({ element: arrowRef })
-        ],
-    })
-
-    const click = useClick(context)
-
-    const { getReferenceProps, getFloatingProps } = useInteractions([
-        click,
-    ]);
-
     return (
         <div className="flex flex-col gap-3">
             <div className="large-text underline">Popups:</div>
-            <Button
-                ref={refs.setReference}
-                {...getReferenceProps()}
-            >
-                Click for auto-placed popup
-            </Button>
-            {isOpen && (
-                <div
-                    ref={refs.setFloating}
-                    className="border border-dark-grey rounded p-4 bg-white"
-                    style={floatingStyles}
-                    {...getFloatingProps()}
-                >
-                    <FloatingArrow
-                        ref={arrowRef}
-                        context={context}
-                        className="
-                        fill-white 
-                        [&>path:first-of-type]:stroke-dark-grey
-                        [&>path:last-of-type]:stroke-dark-grey
-                        "
-                    />
-                    Floating element
-                </div>
-            )}
+            <SimpleAutoplacedExample />
+            <SinglePopupForSetExample />
         </div>
     )
 }
