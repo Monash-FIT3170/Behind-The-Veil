70faa89f20f3ddaefa0b361d04a4936d19d212a2
Nishan Chakma <nishan.chakma.nish@gmail.com>
2024-07-30 08:39:53 +1000

    feature/86cvd53jv fixed schema for artist service location

--- behind-the-veil-siteroot/imports/api/collections/serviceAreaCollection.js
+++
@@ -1,3 +0,0 @@
-import { Mongo } from 'meteor/mongo';
-
-export const ServiceAreaCollection = new Mongo.Collection("service-areas");
--- behind-the-veil-siteroot/imports/api/publications/serviceAreaPublications.js
+++
@@ -1,6 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-import { ServiceAreaCollection } from '/imports/collections/serviceAreaCollection';
-
-Meteor.publish('add_service_area', function publishTasks() {
-    return ServiceAreaCollection.find({ userId: this.userId });
-});
