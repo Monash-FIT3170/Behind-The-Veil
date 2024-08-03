e0b4516684ae70c8ea583ed6f6a0e66e45cc757a
kbib0002 <114703466+kbib0002@users.noreply.github.com>
2024-08-03 15:16:10 +1000

    Feature/86cw0v9uw-change-account-details INTO develop (#82)
    * feature/86cw0v9uw-change-account-details: Added UI
    
    Added all appropriate UI.
    
    What is left to be implemented:
    • save button functionality
    • browse/upload file button functionality
    
    * feature/86cw0v9uw-change-account-details: Added Functionality
    
    Complete functionality has been added. The user can now change their name and email address.
    
    * feature/86cw0v9uw-change-account-details: Added UI
    
    Added all appropriate UI.
    
    What is left to be implemented:
    • save button functionality
    • browse/upload file button functionality
    
    * feature/86cw0v9uw-change-account-details: Added Functionality
    
    Complete functionality has been added. The user can now change their name and email address.
    
    * feature/86cw0v9uw: Modified account details page
    
    * feature/86cw0v9uw: Settings tab style consistency
    
    * feature/86cw0v9uw-change-account-details: code clean
    
    Removed unnecessary imports.
    
    * feature/86cw0v9uw: redid minor overwritten changes
    
    * feature/86cw0v9uw: Added extra input validation for alias
    
    ---------
    
    Co-authored-by: Nikki Li Shao <nsha0054@student.monash.edu>

--- behind-the-veil-siteroot/imports/api/methods/users.js
+++ behind-the-veil-siteroot/imports/api/methods/users.js
@@ -22,6 +22,28 @@ Meteor.methods({
     "verify_email": function (username) {
         const user = Accounts.findUserByUsername(username);
         Accounts.sendVerificationEmail(user._id);
-    }
+    },
+    /**
+     * Changes the email address associated with a user.
+     * @param userId - ID of the user to be updated
+     * @param oldEmail - old email of user to remove
+     * @param newEmail - new email to be added
+     */
+    "update_email": function (userId, oldEmail, newEmail) {
+        // remove old email and add the new one in
+        Accounts.removeEmail(userId, oldEmail)
+        Accounts.addEmail(userId, newEmail);
+   },
+    /**
+     * Changes the alias associated with a user.
+     * @param userId - ID of the user to be updated
+     * @param newAlias - new alias/name to update to
+     */
+    "update_alias": function (userId, newAlias) {
+        UserCollection.update(userId, {$set: {"profile.alias": newAlias}});
+    },
+
+
+
 })
 
