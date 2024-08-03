5fa9a95483f5e8521bc8091173d06919e6ff69df
scp777 <67996361+scp777@users.noreply.github.com>
2024-08-03 16:33:11 +1000

    86cva9dr3: basic modal component completed

---
+++ behind-the-veil-siteroot/imports/ui/components/pages/profile/artistTabs/GalleryModal.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function GalleryModal({ open, onClose }) {
+  return (
+    // backdrop
+    <div
+      onClick={onClose}
+      className={`
+        fixed inset-0 flex justify-center items-center transition-colors
+        ${open ? "visible bg-black/20" : "invisible"}
+      `}
+    >
+      {/* modal */}
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className={`
+          bg-white rounded-xl shadow p-6 transition-all
+          ${open ? "scale-100 opacity-100" : "scale-125 opacity-0"}
+        `}
+      >
+        <div className="text-center w-56">
+          <div className="mx-auto my-4 w-48">
+            <h3 className="text-lg font-black text-gray-800">Confirm Delete</h3>
+            <p className="text-sm text-gray-500">
+              Are you sure you want to delete this item?
+            </p>
+          </div>
+          <div className="flex gap-4"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
