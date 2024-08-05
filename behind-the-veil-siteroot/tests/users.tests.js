9cec1adeacc50fb4f93f493e4a911856dcaee2cd
Nishan Chakma <85787283+cknishan@users.noreply.github.com>
2024-08-05 21:57:56 +1000

    Feature/86cvd53jv save artist service area in settings (#96)
    * feature/86cvd53jv add service location and radius in artist users database schema
    
    * feature/86cvd53jv popup for service area save changes
    
    * feature/86cvd53jv update method for updating the artist profile service location and radius in database
    
    * feature/86cvd53jv test-case for database update; fix display and update method
    
    * feature/86cvd53jv fix conflict
    
    * feature/86cvd53jv updated db field name to be consistent with the schema design
    
    * feature/86cvd53jv error and success message; overlay modal removed
    
    * feature/86cvd53jv handle whitespace in location input
    
    * feature/86cvd53jv JsDoc comment update and remove console log

---
+++ behind-the-veil-siteroot/tests/users.tests.js
@@ -0,0 +1,41 @@
+const assert = require('assert');
+import {resetDatabase} from 'meteor/xolvio:cleaner';
+import "../imports/api/methods/users";
+import {UserCollection} from "../imports/api/collections/users";
+
+/**
+ * Test suite for client-side user methods.
+ */
+if (Meteor.isClient) {
+    /**
+     * Describes test cases for user methods.
+     */
+    describe('User methods', function () {
+        beforeEach(function () {
+            resetDatabase(); // Clear the collection before each test
+        });
+
+        /**
+         * Test case to check if service area can be updated successfully.
+         */
+        it('can update service area', function () {
+            const userId = UserCollection.insert({
+                username: 'testuser',
+                email: 'testuser@example.com',
+                password: 'password',
+                profile: {
+                    alias: 'test',
+                    type: 'artist',
+                    artistServiceLocation: '',
+                    artistserviceRadius: 0,
+                },
+            });
+            Meteor.call('update_service_area', userId, 'clayton', 20 );
+            const updatedUser = UserCollection.findOne(userId);
+            assert.strictEqual(updatedUser.profile.artistServiceLocation, 'clayton');
+            assert.strictEqual(updatedUser.profile.artistServiceRadius, 20);
+        });
+
+
+    });
+}
