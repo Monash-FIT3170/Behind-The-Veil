aa500f1cc3b1b1427c1a7e576dcc1c887ff0b458
scp777 <67996361+scp777@users.noreply.github.com>
2024-08-11 11:32:41 +1000

    86cva9dr3: basic modal structure built (using headless ui component)

--- behind-the-veil-siteroot/imports/ui/components/pages/profile/artistTabs/ArtistGalleryTab.jsx
+++ behind-the-veil-siteroot/imports/ui/components/pages/profile/artistTabs/ArtistGalleryTab.jsx
@@ -9,8 +9,9 @@ import { PlusIcon } from "@heroicons/react/24/outline";
 
 import Button from "../../../button/Button";
 import GalleryModalOld from "./GalleryModalOld";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import { Dialog, Transition } from "@headlessui/react";
 
 import { getGalleryImages } from "../../../DatabaseHelper";
 
@@ -20,36 +21,97 @@ import { getGalleryImages } from "../../../DatabaseHelper";
  * @param username {string} - username of the current user's profile
  */
 export const ArtistGalleryTab = ({ username }) => {
-  const [open, setOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   const [itemsPerPage, setItemsPerPage] = useState(10);
   const plusIcon = <PlusIcon className="icon-base" />;
-  const galleryImgData = getGalleryImages(username);
+  const galleryImgData = getGalleryImages("post_images", [], username);
   console.log(username);
   console.log(galleryImgData);
   // Photos Gallery code: https://www.material-tailwind.com/docs/react/gallery
   // When completing the dynamic version for this page, probably a good idea to setup the photos as components and importing them in.
 
   const handleImageClick = (src) => {
-    setOpen(true);
+    setIsOpen(true);
     console.log(src);
   };
 
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
   return (
     <div className="relative">
-      <div className="sticky z-30">
+      {/* <div className="sticky z-30">
         <GalleryModalOld
-          open={open}
-          onClose={() => setOpen(false)}
+          open={isOpen}
+          onClose={() => setIsOpen(false)}
           children={galleryImgData[1]}
         ></GalleryModalOld>
-      </div>
+      </div> */}
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-30" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black/70" aria-hidden="true" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="grid grid-cols-3 gap-4 w-5/6 transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <div className="col-span-2">
+                    <img
+                      className="w-full h-full object-cover"
+                      src={galleryImgData[1]}
+                    ></img>
+                  </div>
+                  <div>
+                    <Dialog.Title
+                      as="h3"
+                      className="text-xl font-medium leading-6 text-gray-900"
+                    >
+                      Payment successful
+                    </Dialog.Title>
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-500">
+                        Your payment has been successfully submitted. We’ve sent
+                        you an email with all of the details of your order.
+                      </p>
+                    </div>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+
       <div className="sticky top-20 z-20 flex justify-end">
         <Button className="absolute top-5 flex flex-row gap-x-1.5 bg-secondary-purple hover:bg-secondary-purple-hover mt-2">
           {plusIcon} Add Photo
         </Button>
       </div>
       <ResponsiveMasonry>
-        <Masonry>
+        <Masonry gutter="5px">
           {galleryImgData.map((image, i) => (
             <img
               key={i}
@@ -57,9 +119,9 @@ export const ArtistGalleryTab = ({ username }) => {
               style={{
                 width: "100%",
                 display: "block",
-                padding: "5px",
               }}
-              onClick={() => handleImageClick(image)}
+              onClick={openModal}
+              // onClick={() => handleImageClick(image)}
             />
           ))}
         </Masonry>
