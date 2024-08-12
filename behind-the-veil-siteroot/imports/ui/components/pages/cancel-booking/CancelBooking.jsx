0192035402412ce65d173469b8bc03c3fa01bb0b
lzha0181 <lzha0181@student.monash.edu>
2024-08-12 12:00:55 +1000

    feature/86cuwqvt8: connecting db

--- behind-the-veil-siteroot/imports/ui/components/pages/cancel-booking/CancelBooking.jsx
+++ behind-the-veil-siteroot/imports/ui/components/pages/cancel-booking/CancelBooking.jsx
@@ -12,6 +12,13 @@ import PageLayout from "../../../enums/PageLayout";
 import Button from "../../button/Button";
 import {CheckIcon} from "@heroicons/react/24/outline"
 import BackButton from "../../button/BackButton";
+import {useSubscribe, useTracker} from "meteor/react-meteor-data";
+import Loader from "../../loader/Loader";
+import {getUserInfo} from "/imports/ui/components/util"
+
+import BookingCollection from "/imports/api/collections/bookings";
+import ServiceCollection from "../../../../api/collections/services";
+import UserCollection from "../../../../api/collections/users";
 
 /**
  * Page for user to cancel a booking
@@ -26,6 +33,7 @@ const CancelBooking = () => {
     }
 
     const [inputReason, setInputReason] = useState("");
+    const userInfo = getUserInfo();
 
     function handleInputChange(event) {
         setInputReason(event.target.value);
@@ -39,45 +47,103 @@ const CancelBooking = () => {
     // grab the service ID from the URL
     const {bookingId} = useParams();
 
-    return (
-        <WhiteBackground pageLayout={PageLayout.LARGE_CENTER}>
-            <BackButton to={"/profile"}/>
+    // get bookings information from database
+    const isLoadingBooking = useSubscribe('specific_booking', bookingId);
 
-            {/* Main container for content */}
-            <div className="flex flex-col gap-4 xl:px-40">
-                <div className="large-text">Cancel Booking</div>
-                <ServiceDetailsHeader
-                    service={MOCK_CANCEL_BOOKING_DETAILS.service}
-                    date={MOCK_CANCEL_BOOKING_DETAILS.date}
-                    artist={MOCK_CANCEL_BOOKING_DETAILS.artist}
-                    price={MOCK_CANCEL_BOOKING_DETAILS.price}
+    let bookingData = useTracker(() => {
+        return BookingCollection.find().fetch()[0];
+    });
+
+    // load the service's data
+    const isLoadingService = useSubscribe('specific_service', bookingData ? bookingData.serviceId : "");
+
+    // load the artist's data
+    const isLoadingUser = useSubscribe('specific_user', bookingData ? bookingData.artistUsername : "");
+
+    // data trackers
+    let serviceData = useTracker(() => {
+        return ServiceCollection.find().fetch()[0];
+    });
+
+    // data trackers
+    let userData = useTracker(() => {
+        return UserCollection.find().fetch()[0];
+    });
+
+    const isLoading = isLoadingBooking() || isLoadingService() || isLoadingUser();
+
+    if (isLoading) {
+        // is loading, display loader
+        return (
+            <WhiteBackground pageLayout={PageLayout.LARGE_CENTER}>
+                <Loader
+                    loadingText={"loading . . ."}
+                    isLoading={isLoading}
+                    size={100}
+                    speed={1.5}
                 />
+            </WhiteBackground>
+        );
+    } else {
+        // finished loading
+        if (!bookingData || (userInfo.username !== bookingData.artistUsername && userInfo.username !== bookingData.brideUsername)) {
+            // if booking data for this booking ID is not found OR this booking doesn't belong to the current user
+            return (
+                <WhiteBackground>
+                    <div className="flex flex-col gap-y-6 items-center justify-center">
+                        <span className={"large-text"}>Booking is not found </span>
 
-                {/* cancellation reason */}
-                <div className="flex flex-col gap-1">
-                    <label htmlFor="cancellation-input" className="main-text text-our-black">Reason of
-                        cancellation</label>
-                    <textarea id="cancellation-input"
-                              className="input-base h-48"
-                              placeholder="Enter Your Reason"
-                              onChange={handleInputChange}
-                              rows={4} cols={40}/>
-                </div>
-
-                {/* button */}
-                <div className="flex gap-10">
-                    {/* button */}
-                    <div className="flex flex-col gap-4 grow">
-                        <Button className="bg-secondary-purple hover:bg-secondary-purple-hover flex gap-2"
-                                onClick={handleCancelBooking}>
-                            <CheckIcon className="icon-base"/>
-                            Cancel Booking
+                        <Button className={"bg-secondary-purple hover:bg-secondary-purple-hover"}
+                                onClick={() => navigateTo("/profile")}>
+                            Back to my profile
                         </Button>
                     </div>
-                </div>
-            </div>
+                </WhiteBackground>
+            );
+        } else {
+            // convert date string into a date
+            const bookingDatetime = new Date(bookingData.bookingStartDateTime);
+            return (
+                <WhiteBackground pageLayout={PageLayout.LARGE_CENTER}>
+                    <BackButton to={"/profile"}/>
 
-        </WhiteBackground>
-    );
+                    {/* Main container for content */}
+                    <div className="flex flex-col gap-4 xl:px-40">
+                        <div className="large-text">Cancel Booking</div>
+                        <ServiceDetailsHeader
+                            service={serviceData.serviceName}
+                            date={bookingDatetime.toLocaleString()}
+                            artist={userData.profile.alias + " (@" + bookingData.artistUsername + ")"}
+                            price={"$" + bookingData.bookingPrice}
+                        />
+
+                        {/* cancellation reason */}
+                        <div className="flex flex-col gap-1">
+                            <label htmlFor="cancellation-input" className="main-text text-our-black">Reason of
+                                cancellation</label>
+                            <textarea id="cancellation-input"
+                                    className="input-base h-48"
+                                    placeholder="Enter Your Reason"
+                                    onChange={handleInputChange}
+                                    rows={4} cols={40}/>
+                        </div>
+
+                        {/* button */}
+                        <div className="flex gap-10">
+                            {/* button */}
+                            <div className="flex flex-col gap-4 grow">
+                                <Button className="bg-secondary-purple hover:bg-secondary-purple-hover flex gap-2"
+                                        onClick={handleCancelBooking}>
+                                    <CheckIcon className="icon-base"/>
+                                    Cancel Booking
+                                </Button>
+                            </div>
+                        </div>
+                    </div>
+
+                </WhiteBackground>
+            );
+        }
+    }
 };
 export default CancelBooking;
