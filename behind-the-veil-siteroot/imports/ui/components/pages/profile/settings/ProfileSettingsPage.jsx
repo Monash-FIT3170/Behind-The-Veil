76ae615fe3cb9f48a0a60d1750cbdde29be959bb
Nishan Chakma <85787283+cknishan@users.noreply.github.com>
2024-10-06 21:21:17 +1100

    Uat/86cwh98jh payment and service area red dot (#183)
    * UAT/86cwh98jh red dot if payment and service area is not added by artist
    
    * UAT/86cwh98jh removed console log
    
    * UAT/86cwh98jh expose artist service area and payment useUserInfo
    
    * UAT/86cwh98jh fix user type checking

--- behind-the-veil-siteroot/imports/ui/components/pages/profile/settings/ProfileSettingsPage.jsx
+++ behind-the-veil-siteroot/imports/ui/components/pages/profile/settings/ProfileSettingsPage.jsx
@@ -9,17 +9,16 @@ import PageLayout from "../../../../enums/PageLayout";
 import Tabs from "../../../tabs/Tabs.jsx";
 import AccountDetails from "./AccountDetails.jsx";
 import ArtistServiceArea from "./ArtistServiceArea";
-import {useUserInfo} from "../../../util";
 import ChangePasswordTab from "./ChangePasswordTab";
 import BackButton from "../../../button/BackButton";
 import ArtistPayment from "./ArtistPayment.jsx";
 
-
+import {useUserInfo} from "../../../util";
 /**
  * Settings page for all users
  */
 export const ProfileSettingsPage = () => {
-
+    
     // get current user information
     const userInfo = useUserInfo();
 
@@ -27,7 +26,7 @@ export const ProfileSettingsPage = () => {
     if (userInfo.type === "bride") {
         return (
             <WhiteBackground pageLayout={PageLayout.LARGE_CENTER}>
-                <BackButton/>
+                <BackButton />
                 <div className="flex flex-row flex-nowrap items-center">
                     <div className="title-text text-center sm:-mt-10 mb-5 grow">Settings</div>
                 </div>
@@ -37,7 +36,7 @@ export const ProfileSettingsPage = () => {
                         <span key={2}>Change Password</span>,
                     ]}
                     tabPanels={[
-                        <AccountDetails key={"account-details"}/>,
+                        <AccountDetails key={"account-details"} />,
                         <ChangePasswordTab key={"change-password"} />,
                     ]}
                     tabsClassName="lg:flex lg:justify-between lg:px-[25%] xl:px-[30%] 2xl:px-[35%]"
@@ -46,9 +45,11 @@ export const ProfileSettingsPage = () => {
         );
     } else {
         // artist
+        let artistServiceLocation = userInfo.artistServiceLocation;
+        let artistPaymentAccount = userInfo.artistPaymentAccount;
         return (
             <WhiteBackground pageLayout={PageLayout.LARGE_CENTER}>
-                <BackButton/>
+                <BackButton />
                 <div className="flex flex-row flex-nowrap items-center">
                     <div className="title-text text-center sm:-mt-10 mb-5 grow">Settings</div>
                 </div>
@@ -56,15 +57,20 @@ export const ProfileSettingsPage = () => {
                     tabs={[
                         <span key={1}>Account Details</span>,
                         <span key={2}>Change Password</span>,
-                        <span key={3}>Service Area</span>,
-                        <span key={4}>Payment Detail</span>,
+                        <span className="relative" key={3}>Service Area
+                            {(artistServiceLocation ==  null )  && (<span className="absolute top-0 h-2 w-2 bg-red-500 rounded-full"></span>)}
+
+                        </span>,
+                        <span className="relative" key={4}>Payment Detail
+                            {(artistPaymentAccount == null) && (<span className="absolute top-0 h-2 w-2 bg-red-500 rounded-full"></span>)}
+                        </span>,
 
                     ]}
                     tabPanels={[
-                        <AccountDetails key={"account-details"}/>,
+                        <AccountDetails key={"account-details"} />,
                         <ChangePasswordTab key={"change-password"} />,
-                        <ArtistServiceArea key={"service-area"}/>,
-                        <ArtistPayment key={"artist-payment"}/>
+                        <ArtistServiceArea key={"service-area"} />,
+                        <ArtistPayment key={"artist-payment"} />
                     ]}
                     tabsClassName="lg:flex lg:justify-between lg:px-[15%] xl:px-[20%] 2xl:px-[25%]"
                 />
